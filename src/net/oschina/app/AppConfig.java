cfc20e95064ac9156687d39209b4c62c7b5c4a95
叶国忠 <yeguozhong@yeah.net>
2013-05-14 16:37:05 +0800

    加了保存图片的默认路径参数

--- src/net/oschina/app/AppConfig.java
+++ src/net/oschina/app/AppConfig.java
@@ -7,20 +7,24 @@ import java.util.Properties;
 
 import net.oschina.app.bean.AccessInfo;
 import net.oschina.app.common.StringUtils;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.Environment;
 import android.preference.PreferenceManager;
 
 /**
  * 应用程序配置类：用于保存用户相关信息及设置
+ * 
  * @author liux (http://my.oschina.net/liux)
  * @version 1.0
  * @created 2012-3-21
  */
-public class AppConfig{
-	
+@SuppressLint("NewApi")
+public class AppConfig {
+
 	private final static String APP_CONFIG = "config";
-	
+
 	public final static String TEMP_TWEET = "temp_tweet";
 	public final static String TEMP_TWEET_IMAGE = "temp_tweet_image";
 	public final static String TEMP_MESSAGE = "temp_message";
@@ -28,7 +32,7 @@ public class AppConfig{
 	public final static String TEMP_POST_TITLE = "temp_post_title";
 	public final static String TEMP_POST_CATALOG = "temp_post_catalog";
 	public final static String TEMP_POST_CONTENT = "temp_post_content";
-	
+
 	public final static String CONF_APP_UNIQUEID = "APP_UNIQUEID";
 	public final static String CONF_COOKIE = "cookie";
 	public final static String CONF_ACCESSTOKEN = "accessToken";
@@ -39,82 +43,81 @@ public class AppConfig{
 	public final static String CONF_HTTPS_LOGIN = "perf_httpslogin";
 	public final static String CONF_VOICE = "perf_voice";
 	public final static String CONF_CHECKUP = "perf_checkup";
-	
+
+	public final static String SAVE_IMAGE_PATH = "save_image_path";
+	@SuppressLint("NewApi")
+	public final static String DEFAULT_SAVE_IMAGE_PATH = Environment.getExternalStorageDirectory()+ File.separator+ "OSChina"+ File.separator;
+			
 	private Context mContext;
 	private AccessInfo accessInfo = null;
 	private static AppConfig appConfig;
-	
-	public static AppConfig getAppConfig(Context context)
-	{
-		if(appConfig == null){
+
+	public static AppConfig getAppConfig(Context context) {
+		if (appConfig == null) {
 			appConfig = new AppConfig();
 			appConfig.mContext = context;
 		}
 		return appConfig;
 	}
-	
+
 	/**
 	 * 获取Preference设置
 	 */
-	public static SharedPreferences getSharedPreferences(Context context)
-	{
+	public static SharedPreferences getSharedPreferences(Context context) {
 		return PreferenceManager.getDefaultSharedPreferences(context);
 	}
-	
+
 	/**
 	 * 是否加载显示文章图片
 	 */
-	public static boolean isLoadImage(Context context)
-	{
-		return getSharedPreferences(context)
-				.getBoolean(CONF_LOAD_IMAGE, true);
-	}	
-	
-	public String getCookie(){
+	public static boolean isLoadImage(Context context) {
+		return getSharedPreferences(context).getBoolean(CONF_LOAD_IMAGE, true);
+	}
+
+	public String getCookie() {
 		return get(CONF_COOKIE);
 	}
 
-	public void setAccessToken(String accessToken){
+	public void setAccessToken(String accessToken) {
 		set(CONF_ACCESSTOKEN, accessToken);
 	}
-	
-	public String getAccessToken(){
+
+	public String getAccessToken() {
 		return get(CONF_ACCESSTOKEN);
 	}
-	
-	public void setAccessSecret(String accessSecret){
+
+	public void setAccessSecret(String accessSecret) {
 		set(CONF_ACCESSSECRET, accessSecret);
 	}
-	
-	public String getAccessSecret(){
+
+	public String getAccessSecret() {
 		return get(CONF_ACCESSSECRET);
 	}
-	
-	public void setExpiresIn(long expiresIn){
+
+	public void setExpiresIn(long expiresIn) {
 		set(CONF_EXPIRESIN, String.valueOf(expiresIn));
 	}
-	
-	public long getExpiresIn(){
+
+	public long getExpiresIn() {
 		return StringUtils.toLong(get(CONF_EXPIRESIN));
 	}
-	
-	public void setAccessInfo(String accessToken, String accessSecret, long expiresIn)
-	{
-		if(accessInfo == null)
+
+	public void setAccessInfo(String accessToken, String accessSecret,
+			long expiresIn) {
+		if (accessInfo == null)
 			accessInfo = new AccessInfo();
 		accessInfo.setAccessToken(accessToken);
 		accessInfo.setAccessSecret(accessSecret);
 		accessInfo.setExpiresIn(expiresIn);
-		//保存到配置
+		// 保存到配置
 		this.setAccessToken(accessToken);
 		this.setAccessSecret(accessSecret);
 		this.setExpiresIn(expiresIn);
 	}
-	
-	public AccessInfo getAccessInfo()
-	{
-		if(accessInfo == null && !StringUtils.isEmpty(getAccessToken()) && !StringUtils.isEmpty(getAccessSecret()))
-		{
+
+	public AccessInfo getAccessInfo() {
+		if (accessInfo == null && !StringUtils.isEmpty(getAccessToken())
+				&& !StringUtils.isEmpty(getAccessSecret())) {
 			accessInfo = new AccessInfo();
 			accessInfo.setAccessToken(getAccessToken());
 			accessInfo.setAccessSecret(getAccessSecret());
@@ -122,74 +125,73 @@ public class AppConfig{
 		}
 		return accessInfo;
 	}
-	
-	public String get(String key)
-	{
+
+	public String get(String key) {
 		Properties props = get();
-		return (props!=null)?props.getProperty(key):null;
+		return (props != null) ? props.getProperty(key) : null;
 	}
-	
+
 	public Properties get() {
 		FileInputStream fis = null;
 		Properties props = new Properties();
-		try{
-			//读取files目录下的config
-			//fis = activity.openFileInput(APP_CONFIG);
-			
-			//读取app_config目录下的config
+		try {
+			// 读取files目录下的config
+			// fis = activity.openFileInput(APP_CONFIG);
+
+			// 读取app_config目录下的config
 			File dirConf = mContext.getDir(APP_CONFIG, Context.MODE_PRIVATE);
-			fis = new FileInputStream(dirConf.getPath() + File.separator + APP_CONFIG);
-			
+			fis = new FileInputStream(dirConf.getPath() + File.separator
+					+ APP_CONFIG);
+
 			props.load(fis);
-		}catch(Exception e){
-		}finally{
+		} catch (Exception e) {
+		} finally {
 			try {
 				fis.close();
-			} catch (Exception e) {}
+			} catch (Exception e) {
+			}
 		}
 		return props;
 	}
-	
+
 	private void setProps(Properties p) {
 		FileOutputStream fos = null;
-		try{
-			//把config建在files目录下
-			//fos = activity.openFileOutput(APP_CONFIG, Context.MODE_PRIVATE);
-			
-			//把config建在(自定义)app_config的目录下
+		try {
+			// 把config建在files目录下
+			// fos = activity.openFileOutput(APP_CONFIG, Context.MODE_PRIVATE);
+
+			// 把config建在(自定义)app_config的目录下
 			File dirConf = mContext.getDir(APP_CONFIG, Context.MODE_PRIVATE);
 			File conf = new File(dirConf, APP_CONFIG);
 			fos = new FileOutputStream(conf);
-			
+
 			p.store(fos, null);
 			fos.flush();
-		}catch(Exception e){	
+		} catch (Exception e) {
 			e.printStackTrace();
-		}finally{
+		} finally {
 			try {
 				fos.close();
-			} catch (Exception e) {}
+			} catch (Exception e) {
+			}
 		}
 	}
 
-	public void set(Properties ps)
-	{
+	public void set(Properties ps) {
 		Properties props = get();
 		props.putAll(ps);
 		setProps(props);
 	}
-	
-	public void set(String key,String value)
-	{
+
+	public void set(String key, String value) {
 		Properties props = get();
 		props.setProperty(key, value);
 		setProps(props);
 	}
-	
-	public void remove(String...key)
-	{
+
+	public void remove(String... key) {
 		Properties props = get();
-		for(String k : key)
+		for (String k : key)
 			props.remove(k);
 		setProps(props);
 	}
