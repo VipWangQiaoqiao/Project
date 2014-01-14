29d452ec6c0a06361fece07a01a51a13b524a555
貌似掉线 <645079761@qq.com>
2014-01-14 14:12:30 +0800

    optimize code

--- src/net/oschina/app/AppManager.java
+++ src/net/oschina/app/AppManager.java
@@ -8,85 +8,94 @@ import android.content.Context;
 
 /**
  * 应用程序Activity管理类：用于Activity管理和应用程序退出
+ * 
  * @author liux (http://my.oschina.net/liux)
  * @version 1.0
  * @created 2012-3-21
  */
 public class AppManager {
-	
+
 	private static Stack<Activity> activityStack;
 	private static AppManager instance = new AppManager();
-	
-	private AppManager(){}
+
+	private AppManager() {
+		activityStack = new Stack<Activity>();
+	}
+
 	/**
 	 * 单一实例
 	 */
-	public static AppManager getAppManager(){
+	public static AppManager getAppManager() {
 		return instance;
 	}
+
 	/**
 	 * 添加Activity到堆栈
 	 */
-	public void addActivity(Activity activity){
-		if(activityStack==null){
-			activityStack=new Stack<Activity>();
-		}
+	public void addActivity(Activity activity) {
 		activityStack.add(activity);
 	}
+
 	/**
 	 * 获取当前Activity（堆栈中最后一个压入的）
 	 */
-	public Activity currentActivity(){
-		Activity activity=activityStack.lastElement();
+	public Activity currentActivity() {
+		Activity activity = activityStack.lastElement();
 		return activity;
 	}
+
 	/**
 	 * 结束当前Activity（堆栈中最后一个压入的）
 	 */
-	public void finishActivity(){
-		Activity activity=activityStack.lastElement();
+	public void finishActivity() {
+		Activity activity = activityStack.lastElement();
 		finishActivity(activity);
 	}
+
 	/**
 	 * 结束指定的Activity
 	 */
-	public void finishActivity(Activity activity){
-		if(activity!=null){
+	public void finishActivity(Activity activity) {
+		if (activity != null) {
 			activityStack.remove(activity);
 			activity.finish();
-			activity=null;
 		}
 	}
+
 	/**
 	 * 结束指定类名的Activity
 	 */
-	public void finishActivity(Class<?> cls){
+	public void finishActivity(Class<?> cls) {
 		for (Activity activity : activityStack) {
-			if(activity.getClass().equals(cls) ){
+			if (activity.getClass().equals(cls)) {
 				finishActivity(activity);
 			}
 		}
 	}
+
 	/**
 	 * 结束所有Activity
 	 */
-	public void finishAllActivity(){
-		for (int i = 0, size = activityStack.size(); i < size; i++){
-            if (null != activityStack.get(i)){
-            	activityStack.get(i).finish();
-            }
-	    }
+	public void finishAllActivity() {
+		for (Activity activity : activityStack) {
+			if(activity != null ) {
+				activity.finish();
+			}
+		}
 		activityStack.clear();
 	}
+
 	/**
 	 * 退出应用程序
 	 */
 	public void AppExit(Context context) {
 		try {
 			finishAllActivity();
-			ActivityManager activityMgr= (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+			ActivityManager activityMgr = (ActivityManager) context
+					.getSystemService(Context.ACTIVITY_SERVICE);
 			activityMgr.restartPackage(context.getPackageName());
 			System.exit(0);
-		} catch (Exception e) {	}
+		} catch (Exception e) {
+		}
 	}
 }
