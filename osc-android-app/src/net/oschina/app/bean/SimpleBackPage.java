1524b2b8915dcda7cdc2776e2b07b1be14f662ed
kymjs <kymjs123@gmail.com>
2014-11-27 16:59:49 +0800

    添加侧滑菜单中"博客区"模块

--- osc-android-app/src/net/oschina/app/bean/SimpleBackPage.java
+++ osc-android-app/src/net/oschina/app/bean/SimpleBackPage.java
@@ -14,6 +14,7 @@ import net.oschina.app.fragment.TweetPubFragment;
 import net.oschina.app.fragment.UserBlogFragment;
 import net.oschina.app.fragment.UserCenterFragment;
 import net.oschina.app.viewpagefragment.ActiveViewPagerFragment;
+import net.oschina.app.viewpagefragment.BlogViewPagerFragment;
 import net.oschina.app.viewpagefragment.FriendsViewPagerFragment;
 import net.oschina.app.viewpagefragment.OpensourceSoftwareFragment;
 import net.oschina.app.viewpagefragment.QuestViewPagerFragment;
@@ -21,79 +22,90 @@ import net.oschina.app.viewpagefragment.UserFavoriteViewPageFragment;
 
 public enum SimpleBackPage {
 
-	COMMENT(1, R.string.actionbar_title_comment, CommentFrament.class),
-	
-	QUEST(2, R.string.actionbar_title_questions, QuestViewPagerFragment.class),
-	
-	TWEET_PUB(3, R.string.actionbar_title_tweetpub, TweetPubFragment.class),
-	
-	SOFTWARE_TWEETS(4, R.string.actionbar_title_softtweet, SoftWareTweetsFrament.class),
-	
-	USER_CENTER(5, R.string.actionbar_title_user_center, UserCenterFragment.class),
-	
-	USER_BLOG(6, R.string.actionbar_title_user_blog, UserBlogFragment.class),
-	
-	MY_INFORMATION(7, R.string.actionbar_title_my_information, MyInformationFragment.class),
-	
-	MY_ACTIVE(8, R.string.actionbar_title_active, ActiveFragment.class),
-	
-	MY_MES(9, R.string.actionbar_title_mes, ActiveViewPagerFragment.class),
-	
-	OPENSOURCE_SOFTWARE(10, R.string.actionbar_title_softwarelist, OpensourceSoftwareFragment.class),
-	
-	MY_FRIENDS(11, R.string.actionbar_title_my_friends, FriendsViewPagerFragment.class),
-	
-	QUESTION_TAG(12, R.string.actionbar_title_question, QuestionTagFragment.class),
-	
-	MESSAGE_DETAIL(13, R.string.actionbar_title_message_detail, MessageDetailFragment.class),
-	
-	USER_FAVORITE(14, R.string.actionbar_title_user_favorite, UserFavoriteViewPageFragment.class),
-	
-	SETTING(15, R.string.actionbar_title_setting, SettingsFragment.class),
-	
-	SETTING_NOTIFICATION(16, R.string.actionbar_title_setting_notification, SettingsNotificationFragment.class),
-	
-	ABOUT_OSC(17, R.string.actionbar_title_about_osc, AboutOSCFragment.class);
-		
-	private int title;
-	private Class<?> clz;
-	private int value;
+    COMMENT(1, R.string.actionbar_title_comment, CommentFrament.class),
 
-	private SimpleBackPage(int value, int title, Class<?> clz) {
-		this.value = value;
-		this.title = title;
-		this.clz = clz;
-	}
+    QUEST(2, R.string.actionbar_title_questions, QuestViewPagerFragment.class),
 
-	public int getTitle() {
-		return title;
-	}
+    TWEET_PUB(3, R.string.actionbar_title_tweetpub, TweetPubFragment.class),
 
-	public void setTitle(int title) {
-		this.title = title;
-	}
+    SOFTWARE_TWEETS(4, R.string.actionbar_title_softtweet,
+            SoftWareTweetsFrament.class),
 
-	public Class<?> getClz() {
-		return clz;
-	}
+    USER_CENTER(5, R.string.actionbar_title_user_center,
+            UserCenterFragment.class),
 
-	public void setClz(Class<?> clz) {
-		this.clz = clz;
-	}
+    USER_BLOG(6, R.string.actionbar_title_user_blog, UserBlogFragment.class),
 
-	public int getValue() {
-		return value;
-	}
+    MY_INFORMATION(7, R.string.actionbar_title_my_information,
+            MyInformationFragment.class),
 
-	public void setValue(int value) {
-		this.value = value;
-	}
+    MY_ACTIVE(8, R.string.actionbar_title_active, ActiveFragment.class),
 
-	public static SimpleBackPage getPageByValue(int val) {
-		for (SimpleBackPage p : values()) {
-			if (p.getValue() == val)
-				return p;
-		}
-		return null;
-	}
+    MY_MES(9, R.string.actionbar_title_mes, ActiveViewPagerFragment.class),
+
+    OPENSOURCE_SOFTWARE(10, R.string.actionbar_title_softwarelist,
+            OpensourceSoftwareFragment.class),
+
+    MY_FRIENDS(11, R.string.actionbar_title_my_friends,
+            FriendsViewPagerFragment.class),
+
+    QUESTION_TAG(12, R.string.actionbar_title_question,
+            QuestionTagFragment.class),
+
+    MESSAGE_DETAIL(13, R.string.actionbar_title_message_detail,
+            MessageDetailFragment.class),
+
+    USER_FAVORITE(14, R.string.actionbar_title_user_favorite,
+            UserFavoriteViewPageFragment.class),
+
+    SETTING(15, R.string.actionbar_title_setting, SettingsFragment.class),
+
+    SETTING_NOTIFICATION(16, R.string.actionbar_title_setting_notification,
+            SettingsNotificationFragment.class),
+
+    ABOUT_OSC(17, R.string.actionbar_title_about_osc, AboutOSCFragment.class),
+
+    BLOG(18, R.string.actionbar_title_blog_area, BlogViewPagerFragment.class);
+
+    private int title;
+    private Class<?> clz;
+    private int value;
+
+    private SimpleBackPage(int value, int title, Class<?> clz) {
+        this.value = value;
+        this.title = title;
+        this.clz = clz;
+    }
+
+    public int getTitle() {
+        return title;
+    }
+
+    public void setTitle(int title) {
+        this.title = title;
+    }
+
+    public Class<?> getClz() {
+        return clz;
+    }
+
+    public void setClz(Class<?> clz) {
+        this.clz = clz;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public static SimpleBackPage getPageByValue(int val) {
+        for (SimpleBackPage p : values()) {
+            if (p.getValue() == val)
+                return p;
+        }
+        return null;
+    }
 }
--- osc-android-app/src/net/oschina/app/fragment/BlogFragment.java
+++ osc-android-app/src/net/oschina/app/fragment/BlogFragment.java
@@ -10,54 +10,56 @@ import net.oschina.app.base.ListBaseAdapter;
 import net.oschina.app.bean.Blog;
 import net.oschina.app.bean.BlogList;
 import net.oschina.app.bean.ListEntity;
-import net.oschina.app.util.TLog;
 import net.oschina.app.util.UIHelper;
 import net.oschina.app.util.XmlUtils;
 import android.view.View;
 import android.widget.AdapterView;
 
 /**
- * @author HuangWenwei
- *
- * @date 2014年10月10日
+ * 博客区中单一模块的展示
+ * 
+ * @author kymjs(kymjs123@gmail.com)
  */
 public class BlogFragment extends BaseListFragment {
-	
-	protected static final String TAG = BlogFragment.class.getSimpleName();
-	private static final String CACHE_KEY_PREFIX = "bloglist_";
 
-	@Override
-	protected ListBaseAdapter getListAdapter() {
-		return new BlogAdapter();
-	}
+    protected static final String TAG = BlogFragment.class.getSimpleName();
+    private static final String CACHE_KEY_PREFIX = "bloglist_";
 
-	@Override
-	protected String getCacheKeyPrefix() {
-		return CACHE_KEY_PREFIX + blogType;
-	}
+    @Override
+    protected ListBaseAdapter getListAdapter() {
+        return new BlogAdapter();
+    }
 
-	@Override
-	protected ListEntity parseList(InputStream is) throws Exception {
-		BlogList list = XmlUtils.toBean(BlogList.class, is);
-		return list;
-	}
+    /**
+     * 获取当前展示页面的缓存数据
+     */
+    @Override
+    protected String getCacheKeyPrefix() {
+        return CACHE_KEY_PREFIX + blogType;
+    }
 
-	@Override
-	protected ListEntity readList(Serializable seri) {
-		return ((BlogList) seri);
-	}
+    @Override
+    protected ListEntity parseList(InputStream is) throws Exception {
+        BlogList list = XmlUtils.toBean(BlogList.class, is);
+        return list;
+    }
 
-	@Override
-	protected void sendRequestData() {
-		OSChinaApi.getBlogList(blogType, mCurrentPage, mHandler);
-	}
+    @Override
+    protected ListEntity readList(Serializable seri) {
+        return ((BlogList) seri);
+    }
 
-	@Override
-	public void onItemClick(AdapterView<?> parent, View view, int position,
-			long id) {
-		Blog blog = (Blog) mAdapter.getItem(position);
-		if (blog != null)
-			UIHelper.showUrlRedirect(view.getContext(), blog.getUrl());
-	}
+    @Override
+    protected void sendRequestData() {
+        OSChinaApi.getBlogList(blogType, mCurrentPage, mHandler);
+    }
 
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position,
+            long id) {
+        Blog blog = (Blog) mAdapter.getItem(position);
+        if (blog != null) {
+            UIHelper.showUrlRedirect(view.getContext(), blog.getUrl());
+        }
+    }
 }
