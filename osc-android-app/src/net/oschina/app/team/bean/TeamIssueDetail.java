00d5e73e5ea0125fa875a9610da403739b590fba
火蚁 <fireant.me@gmail.com>
2015-01-28 09:48:48 +0800

    获取任务详情

---
+++ osc-android-app/src/net/oschina/app/team/bean/TeamIssueDetail.java
@@ -0,0 +1,21 @@
+package net.oschina.app.team.bean;
+
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+
+import net.oschina.app.bean.Entity;
+
+/**
+ * 团队任务实体类
+ * 
+ * TeamIssueDetail.java
+ * 
+ * @author 火蚁(http://my.oschina.net/u/253900)
+ *
+ * @data 2015-1-27 下午7:44:38
+ */
+@SuppressWarnings("serial")
+@XStreamAlias("oschina")
+public class TeamIssueDetail extends Entity {
+    
+}
+
