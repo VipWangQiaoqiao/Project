3e741ee6f7784cc395e49da5a1b9580971c1638c
火蚁 <fireant.me@gmail.com>
2015-01-31 10:03:09 +0800

    修复发送文字动弹提示失败的问题

--- osc-android-app/src/net/oschina/app/bean/ResultBean.java
+++ osc-android-app/src/net/oschina/app/bean/ResultBean.java
@@ -2,57 +2,58 @@ package net.oschina.app.bean;
 
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 
-/** 
+/**
  * 操作结果实体类
+ * 
  * @author FireAnt（http://my.oschina.net/LittleDY）
- * @version 创建时间：2014年10月14日 下午2:59:27 
+ * @version 创建时间：2014年10月14日 下午2:59:27
  * 
  */
 @SuppressWarnings("serial")
 @XStreamAlias("oschina")
 public class ResultBean extends Base {
-	
-	@XStreamAlias("result")
-	private Result result;
-	
-	@XStreamAlias("notice")
-	private Notice notice;
-	
-	@XStreamAlias("comment")
-	private Comment comment;
-	
-	@XStreamAlias("relation")
-	private int relation;
 
-	public Result getResult() {
-		return result;
-	}
+    @XStreamAlias("result")
+    private Result result;
 
-	public void setResult(Result result) {
-		this.result = result;
-	}
+    @XStreamAlias("notice")
+    private Notice notice;
 
-	public int getRelation() {
-		return relation;
-	}
+    @XStreamAlias("comment")
+    private Comment comment;
 
-	public void setRelation(int relation) {
-		this.relation = relation;
-	}
+    @XStreamAlias("relation")
+    private int relation;
 
-	public Notice getNotice() {
-		return notice;
-	}
+    public Result getResult() {
+	return result;
+    }
 
-	public void setNotice(Notice notice) {
-		this.notice = notice;
-	}
-	
-	public Comment getComment() {
-		return comment;
-	}
+    public void setResult(Result result) {
+	this.result = result;
+    }
 
-	public void setComment(Comment comment) {
-		this.comment = comment;
-	}
+    public int getRelation() {
+	return relation;
+    }
+
+    public void setRelation(int relation) {
+	this.relation = relation;
+    }
+
+    public Notice getNotice() {
+	return notice;
+    }
+
+    public void setNotice(Notice notice) {
+	this.notice = notice;
+    }
+
+    public Comment getComment() {
+	return comment;
+    }
+
+    public void setComment(Comment comment) {
+	this.comment = comment;
+    }
 }
