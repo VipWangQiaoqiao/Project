b38ebf77e8247160510601ee4272f319ed494a1d
kymjs <kymjs123@gmail.com>
2015-03-02 14:06:20 +0800

    修改周报样式

--- osc-android-app/src/net/oschina/app/team/adapter/TeamDiaryAdapter.java
+++ osc-android-app/src/net/oschina/app/team/adapter/TeamDiaryAdapter.java
@@ -14,41 +14,44 @@ import butterknife.InjectView;
 
 public class TeamDiaryAdapter extends ListBaseAdapter<TeamDiary> {
 
-	@Override
-	protected View getRealView(int position, View convertView, ViewGroup parent) {
-		ViewHolder vh = null;
-		if (convertView == null || convertView.getTag() == null) {
-			convertView = getLayoutInflater(parent.getContext()).inflate(
-					R.layout.list_cell_team_diary, null);
-			vh = new ViewHolder(convertView);
-			convertView.setTag(vh);
-		} else {
-			vh = (ViewHolder) convertView.getTag();
-		}
+    @Override
+    protected View getRealView(int position, View convertView, ViewGroup parent) {
+        ViewHolder vh = null;
+        if (convertView == null || convertView.getTag() == null) {
+            convertView = getLayoutInflater(parent.getContext()).inflate(
+                    R.layout.list_cell_team_diary, null);
+            vh = new ViewHolder(convertView);
+            convertView.setTag(vh);
+        } else {
+            vh = (ViewHolder) convertView.getTag();
+        }
 
-		TeamDiary item = (TeamDiary) mDatas.get(position);
-		
-		vh.face.setAvatarUrl(item.getAuthor().getPortrait());
-		vh.author.setText(item.getAuthor().getName());
-		vh.title.setText(Html.fromHtml(item.getTitle().trim()));
-		vh.time.setText(StringUtils.friendly_time(item.getCreateTime()));
-		vh.comment_count.setText(item.getReply() + "");
-		
-		return convertView;
-	}
-	
-	static class ViewHolder {
-		
-		@InjectView(R.id.tv_author) TextView author;
-		@InjectView(R.id.tv_title) TextView title;
-		@InjectView(R.id.tv_date) TextView time;
-		@InjectView(R.id.tv_count) TextView comment_count;
-		
-		@InjectView(R.id.iv_face)
-		public AvatarView face;		
-		
-		public ViewHolder(View view) {
-			ButterKnife.inject(this,view);
-		}
-	}
+        TeamDiary item = mDatas.get(position);
+
+        vh.face.setAvatarUrl(item.getAuthor().getPortrait());
+        vh.author.setText(item.getAuthor().getName());
+        vh.title.setText(Html.fromHtml(item.getTitle().trim()));
+        vh.time.setText(StringUtils.friendly_time(item.getCreateTime()));
+        vh.comment_count.setText(item.getReply() + "");
+
+        return convertView;
+    }
+
+    static class ViewHolder {
+        @InjectView(R.id.tv_author)
+        TextView author;
+        @InjectView(R.id.tv_title)
+        TextView title;
+        @InjectView(R.id.tv_date)
+        TextView time;
+        @InjectView(R.id.tv_count)
+        TextView comment_count;
+
+        @InjectView(R.id.iv_face)
+        public AvatarView face;
+
+        public ViewHolder(View view) {
+            ButterKnife.inject(this, view);
+        }
+    }
 }
