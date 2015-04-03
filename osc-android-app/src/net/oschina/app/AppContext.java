fc749510be378f04a1185cda73393a8ddb13ff01
火蚁 <fireant.me@gmail.com>
2015-04-03 16:59:55 +0800

    打包2.1版本

--- osc-android-app/src/net/oschina/app/AppContext.java
+++ osc-android-app/src/net/oschina/app/AppContext.java
@@ -50,8 +50,8 @@ public class AppContext extends BaseApplication {
     public void onCreate() {
         super.onCreate();
         // 注册App异常崩溃处理器
-        // Thread.setDefaultUncaughtExceptionHandler(AppException
-        // .getAppExceptionHandler(this));
+         Thread.setDefaultUncaughtExceptionHandler(AppException
+         .getAppExceptionHandler(this));
         instance = this;
         init();
         initLogin();
