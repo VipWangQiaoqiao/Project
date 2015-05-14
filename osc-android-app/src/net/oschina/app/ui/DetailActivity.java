8413c3c3b34eac078f4eb18b53676cacf6b0ef82
kymjs <kymjs123@gmail.com>
2015-05-14 11:17:40 +0800

    任务详情界面Emoji表情栏隐藏

--- osc-android-app/src/net/oschina/app/ui/DetailActivity.java
+++ osc-android-app/src/net/oschina/app/ui/DetailActivity.java
@@ -143,6 +143,7 @@ public class DetailActivity extends BaseActivity implements OnSendClickListener 
         if (currentFragment instanceof TweetDetailFragment
                 || currentFragment instanceof TeamTweetDetailFragment
                 || currentFragment instanceof TeamDiaryDetail
+                || currentFragment instanceof TeamIssueDetailFragment
                 || currentFragment instanceof TeamDiscussDetailFragment) {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.emoji_keyboard, emojiFragment).commit();
