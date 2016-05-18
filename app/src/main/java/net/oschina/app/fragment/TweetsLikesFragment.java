b9d8e4e1e210cdd141ce15e42e06e1a9117fc355
unicorn <287890808@qq.com>
2016-05-18 17:16:48 +0800

    修复赞过我显示重复bug

--- app/src/main/java/net/oschina/app/fragment/TweetsLikesFragment.java
+++ app/src/main/java/net/oschina/app/fragment/TweetsLikesFragment.java
@@ -1,8 +1,12 @@
 package net.oschina.app.fragment;
 
-import java.io.InputStream;
-import java.io.Serializable;
-import java.util.List;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 
 import net.oschina.app.AppContext;
 import net.oschina.app.R;
@@ -21,13 +25,10 @@ import net.oschina.app.ui.empty.EmptyLayout;
 import net.oschina.app.util.UIHelper;
 import net.oschina.app.util.XmlUtils;
 import net.oschina.app.viewpagerfragment.NoticeViewPagerFragment;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
+
+import java.io.InputStream;
+import java.io.Serializable;
+import java.util.List;
 
 /**
  * 赞过我动弹的列表
@@ -36,136 +37,135 @@ import android.widget.AdapterView;
  */
 public class TweetsLikesFragment extends BaseListFragment<TweetLike> {
 
-    protected static final String TAG = TweetsLikesFragment.class
-	    .getSimpleName();
     private static final String CACHE_KEY_PREFIX = "mytweets_like_list_";
 
     private boolean mIsWatingLogin;
+    private BroadcastReceiver mReceiver = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            setupContent();
+        }
+    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-	super.onCreate(savedInstanceState);
-	if (mCatalog > 0) {
-	    IntentFilter filter = new IntentFilter(
-		    Constants.INTENT_ACTION_USER_CHANGE);
-	    filter.addAction(Constants.INTENT_ACTION_LOGOUT);
-	    getActivity().registerReceiver(mReceiver, filter);
-	}
+        super.onCreate(savedInstanceState);
+        if (mCatalog > 0) {
+            IntentFilter filter = new IntentFilter(
+                    Constants.INTENT_ACTION_USER_CHANGE);
+            filter.addAction(Constants.INTENT_ACTION_LOGOUT);
+            getActivity().registerReceiver(mReceiver, filter);
+        }
     }
 
     @Override
     public void onDestroy() {
-	if (mCatalog > 0) {
-	    getActivity().unregisterReceiver(mReceiver);
-	}
-	super.onDestroy();
+        if (mCatalog > 0) {
+            getActivity().unregisterReceiver(mReceiver);
+        }
+        super.onDestroy();
     }
 
     @Override
     protected TweetLikeAdapter getListAdapter() {
-	return new TweetLikeAdapter();
+        return new TweetLikeAdapter();
     }
 
     @Override
     protected String getCacheKeyPrefix() {
-	return CACHE_KEY_PREFIX + mCatalog;
+        return CACHE_KEY_PREFIX + mCatalog;
     }
 
     @Override
     protected TweetLikeList parseList(InputStream is) throws Exception {
-	TweetLikeList list = XmlUtils.toBean(TweetLikeList.class, is);
-	return list;
+        TweetLikeList list = XmlUtils.toBean(TweetLikeList.class, is);
+        return list;
     }
 
     @Override
     protected TweetLikeList readList(Serializable seri) {
-	return ((TweetLikeList) seri);
+        return ((TweetLikeList) seri);
     }
 
     @Override
     protected void sendRequestData() {
-	OSChinaApi.getTweetLikeList(mHandler);
+        OSChinaApi.getTweetLikeList(mCurrentPage, mHandler);
     }
+
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position,
-	    long id) {
-		TweetLike tweetLike = mAdapter.getItem(position);
-		if(tweetLike != null){
-			Tweet tweet = tweetLike.getTweet();
-			UIHelper.showTweetDetail(view.getContext(), null, tweet.getId());
-		}
+                            long id) {
+        TweetLike tweetLike = mAdapter.getItem(position);
+        if (tweetLike != null) {
+            Tweet tweet = tweetLike.getTweet();
+            UIHelper.showTweetDetail(view.getContext(), null, tweet.getId());
+        }
     }
 
-    private BroadcastReceiver mReceiver = new BroadcastReceiver() {
-
-	@Override
-	public void onReceive(Context context, Intent intent) {
-	    setupContent();
-	}
-    };
-
     private void setupContent() {
-	if (AppContext.getInstance().isLogin()) {
-	    mIsWatingLogin = false;
-	    mErrorLayout.setErrorType(EmptyLayout.NETWORK_LOADING);
-	    requestData(true);
-	} else {
-	    mCatalog = TweetsList.CATALOG_ME;
-	    mIsWatingLogin = true;
-	    mErrorLayout.setErrorType(EmptyLayout.NETWORK_ERROR);
-	    mErrorLayout.setErrorMessage(getString(R.string.unlogin_tip));
-	}
+        if (AppContext.getInstance().isLogin()) {
+            mIsWatingLogin = false;
+            mErrorLayout.setErrorType(EmptyLayout.NETWORK_LOADING);
+            requestData(true);
+        } else {
+            mCatalog = TweetsList.CATALOG_ME;
+            mIsWatingLogin = true;
+            mErrorLayout.setErrorType(EmptyLayout.NETWORK_ERROR);
+            mErrorLayout.setErrorMessage(getString(R.string.unlogin_tip));
+        }
     }
 
     @Override
     protected void requestData(boolean refresh) {
-	if (AppContext.getInstance().isLogin()) {
-	    mCatalog = AppContext.getInstance().getLoginUid();
-	    mIsWatingLogin = false;
-	    super.requestData(refresh);
-	} else {
-	    mIsWatingLogin = true;
-	    mErrorLayout.setErrorType(EmptyLayout.NETWORK_ERROR);
-	    mErrorLayout.setErrorMessage(getString(R.string.unlogin_tip));
-	}
+        if (AppContext.getInstance().isLogin()) {
+            mCatalog = AppContext.getInstance().getLoginUid();
+            mIsWatingLogin = false;
+            super.requestData(refresh);
+        } else {
+            mIsWatingLogin = true;
+            mErrorLayout.setErrorType(EmptyLayout.NETWORK_ERROR);
+            mErrorLayout.setErrorMessage(getString(R.string.unlogin_tip));
+        }
     }
 
     @Override
     public void initView(View view) {
-	super.initView(view);
-	mErrorLayout.setOnLayoutClickListener(new View.OnClickListener() {
+        super.initView(view);
+        mErrorLayout.setOnLayoutClickListener(new View.OnClickListener() {
 
-	    @Override
-	    public void onClick(View v) {
-		if (AppContext.getInstance().isLogin()) {
-		    mErrorLayout.setErrorType(EmptyLayout.NETWORK_LOADING);
-		    requestData(true);
-		} else {
-		    UIHelper.showLoginActivity(getActivity());
-		}
-	    }
-	});
+            @Override
+            public void onClick(View v) {
+                if (AppContext.getInstance().isLogin()) {
+                    mErrorLayout.setErrorType(EmptyLayout.NETWORK_LOADING);
+                    requestData(true);
+                } else {
+                    UIHelper.showLoginActivity(getActivity());
+                }
+            }
+        });
     }
 
     @Override
     protected long getAutoRefreshTime() {
-	if (mCatalog == TweetsList.CATALOG_LATEST) {
-	    return 3 * 60;
-	}
-	return super.getAutoRefreshTime();
+        if (mCatalog == TweetsList.CATALOG_LATEST) {
+            return 3 * 60;
+        }
+        return super.getAutoRefreshTime();
     }
 
     @Override
     protected void onRefreshNetworkSuccess() {
-	// TODO Auto-generated method stub
-	super.onRefreshNetworkSuccess();
-	if (AppContext.getInstance().isLogin()
-		&& mCatalog == AppContext.getInstance().getLoginUid()
-		&& 4 == NoticeViewPagerFragment.sCurrentPage) {
-	    NoticeUtils.clearNotice(Notice.TYPE_NEWLIKE);
-	    UIHelper.sendBroadcastForNotice(getActivity());
-	}
+        // TODO Auto-generated method stub
+
+        super.onRefreshNetworkSuccess();
+        if (AppContext.getInstance().isLogin()
+                && mCatalog == AppContext.getInstance().getLoginUid()
+                && 4 == NoticeViewPagerFragment.sCurrentPage) {
+            NoticeUtils.clearNotice(Notice.TYPE_NEWLIKE);
+            UIHelper.sendBroadcastForNotice(getActivity());
+        }
     }
 
     protected boolean compareTo(List<? extends Entity> data, Entity enity) {
@@ -174,7 +174,7 @@ public class TweetsLikesFragment extends BaseListFragment<TweetLike> {
         if (enity != null && enity instanceof TweetLike) {
             TweetLike tweetLike = (TweetLike) enity;
             for (int i = 0; i < s; i++) {
-                if (tweetLike.getUser().getId() == ((TweetLike)data.get(i)).getId()) {
+                if (tweetLike.getUser().getId() == ((TweetLike) data.get(i)).getId()) {
                     return true;
                 }
             }
