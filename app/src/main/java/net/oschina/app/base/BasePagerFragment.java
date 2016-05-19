c618dcbfb8ef5fbcc3169d5e596b95ca08aa1042
huanghaibin_dev <huanghaibin_dev@163.com>
2016-05-19 13:07:37 +0800

    Init the GeneralPagerFragment and child fragment

---
+++ app/src/main/java/net/oschina/app/base/BasePagerFragment.java
@@ -0,0 +1,24 @@
+package net.oschina.app.base;
+
+import net.oschina.app.R;
+
+/**
+ * Created by huanghaibin
+ * on 16-5-19.
+ */
+public abstract class BasePagerFragment extends BaseCommonFragment{
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_detail;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
