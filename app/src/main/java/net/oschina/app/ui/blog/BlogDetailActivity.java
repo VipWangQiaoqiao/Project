cfd2b6371758d374953c4d20b7933ccaeb14acc7
qiujuer <qiujuer@live.cn>
2016-05-28 15:13:30 +0800

    move blog detail view to fragment but the net logic save to activity

--- app/src/main/java/net/oschina/app/ui/blog/BlogDetailActivity.java
+++ app/src/main/java/net/oschina/app/ui/blog/BlogDetailActivity.java
@@ -3,24 +3,13 @@ package net.oschina.app.ui.blog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.view.KeyEvent;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
-import android.view.inputmethod.InputMethodManager;
-import android.webkit.WebView;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.RequestManager;
 import com.google.gson.reflect.TypeToken;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.TextHttpResponseHandler;
@@ -31,9 +20,9 @@ import net.oschina.app.api.remote.OSChinaApi;
 import net.oschina.app.bean.Result;
 import net.oschina.app.bean.base.ResultBean;
 import net.oschina.app.bean.blog.BlogDetail;
-import net.oschina.app.util.FontSizeUtils;
-import net.oschina.app.util.ThemeSwitchUtils;
-import net.oschina.app.util.UIHelper;
+import net.oschina.app.contract.BlogDetailContract;
+import net.oschina.app.fragment.general.BlogDetailFragment;
+import net.oschina.app.ui.empty.EmptyLayout;
 import net.oschina.app.util.XmlUtils;
 
 import java.io.ByteArrayInputStream;
@@ -41,28 +30,11 @@ import java.lang.reflect.Type;
 
 import cz.msebera.android.httpclient.Header;
 
-public class BlogDetailActivity extends AppCompatActivity implements View.OnClickListener {
-    private RequestManager mImgLoader;
+public class BlogDetailActivity extends AppCompatActivity implements BlogDetailContract.Operator {
     private long mId;
-    private WebView mWebView;
-    private TextView mTVAuthorName;
-    private TextView mTVPubDate;
-    private TextView mTVTitle;
-    private TextView mTVCommentCount;
-    private ImageView mIVLabelRecommend;
-    private ImageView mIVLabelOriginate;
-    private ImageView mIVAuthorPortrait;
-    private Button mBtnRelation;
-    private EditText mETInput;
-
-    private ImageView mIVFlow;
-    private ImageView mIVShare;
-
-    private LinearLayout mLayAbouts;
-    private LinearLayout mLayComments;
-
-    private long mCommentId;
+    private EmptyLayout mEmptyLayout;
     private BlogDetail mBlog;
+    private BlogDetailContract.View mView;
 
     public static void show(Context context, long id) {
         Intent intent = new Intent(context, BlogDetailActivity.class);
@@ -81,12 +53,20 @@ public class BlogDetailActivity extends AppCompatActivity implements View.OnClic
             actionBar.setHomeButtonEnabled(false);
         }
 
-        mId = mCommentId = getIntent().getLongExtra("id", 0);
+        mId = getIntent().getLongExtra("id", 0);
         if (mId == 0)
             finish();
         else {
-            initView();
-            initData();
+            mEmptyLayout = (EmptyLayout) findViewById(R.id.lay_error);
+            mEmptyLayout.setOnLayoutClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mEmptyLayout.setErrorType(EmptyLayout.NETWORK_LOADING);
+                    initData();
+                }
+            });
+            //initData();
+            showBlog();
         }
     }
 
@@ -99,8 +79,8 @@ public class BlogDetailActivity extends AppCompatActivity implements View.OnClic
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_blog_detail, menu);
+        //MenuInflater inflater = getMenuInflater();
+        //inflater.inflate(R.menu.menu_blog_detail, menu);
         return true;
     }
 
@@ -109,52 +89,25 @@ public class BlogDetailActivity extends AppCompatActivity implements View.OnClic
         return super.onOptionsItemSelected(item);
     }
 
-    private void initView() {
-        mImgLoader = Glide.with(this);
+    private void showBlog() {
+        BlogDetailFragment fragment = BlogDetailFragment.instantiate(this, mBlog);
+        FragmentTransaction trans = getSupportFragmentManager()
+                .beginTransaction();
+        trans.replace(R.id.lay_container, fragment);
+        trans.commitAllowingStateLoss();
+        mView = fragment;
+    }
 
-        mWebView = (WebView) findViewById(R.id.webview);
-        UIHelper.initWebView(mWebView);
-
-        mTVAuthorName = (TextView) findViewById(R.id.tv_name);
-        mTVPubDate = (TextView) findViewById(R.id.tv_pub_date);
-        mTVTitle = (TextView) findViewById(R.id.tv_title);
-        mTVCommentCount = (TextView) findViewById(R.id.tv_comment_count);
-
-        mIVLabelRecommend = (ImageView) findViewById(R.id.iv_label_recommend);
-        mIVLabelOriginate = (ImageView) findViewById(R.id.iv_label_originate);
-        mIVAuthorPortrait = (ImageView) findViewById(R.id.iv_avatar);
-
-        mBtnRelation = (Button) findViewById(R.id.btn_relation);
-        mETInput = (EditText) findViewById(R.id.et_input);
-
-        mIVFlow = (ImageView) findViewById(R.id.iv_flow);
-        mIVShare = (ImageView) findViewById(R.id.iv_share);
-
-        mLayAbouts = (LinearLayout) findViewById(R.id.lay_blog_detail_about);
-        mLayComments = (LinearLayout) findViewById(R.id.lay_blog_detail_comment);
-
-        mIVFlow.setOnClickListener(this);
-        mIVShare.setOnClickListener(this);
-        mETInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_SEND) {
-                    InputMethodManager imm = (InputMethodManager) v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
-                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
-
-
-                    return true;
-                }
-                return false;
-            }
-        });
+    private void showError(int type) {
+        mEmptyLayout.setErrorType(type);
+        mEmptyLayout.setVisibility(View.VISIBLE);
     }
 
     private void initData() {
         OSChinaApi.getBlogDetail(mId, new TextHttpResponseHandler() {
             @Override
             public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
-
+                showError(EmptyLayout.NETWORK_ERROR);
             }
 
             @Override
@@ -166,7 +119,9 @@ public class BlogDetailActivity extends AppCompatActivity implements View.OnClic
                     ResultBean<BlogDetail> resultBean = AppContext.createGson().fromJson(responseString, type);
                     if (resultBean != null && resultBean.isSuccess()) {
                         handleData(resultBean.getResult());
+                        return;
                     }
+                    showError(EmptyLayout.NODATA);
                 } catch (Exception e) {
                     e.printStackTrace();
                     onFailure(statusCode, headers, responseString, e);
@@ -175,113 +130,26 @@ public class BlogDetailActivity extends AppCompatActivity implements View.OnClic
         });
     }
 
-    private final static String linkCss = "<script type=\"text/javascript\" " +
-            "src=\"file:///android_asset/shCore.js\"></script>"
-            + "<script type=\"text/javascript\" src=\"file:///android_asset/brush.js\"></script>"
-            + "<script type=\"text/javascript\" src=\"file:///android_asset/client.js\"></script>"
-            + "<script type=\"text/javascript\" src=\"file:///android_asset/detail_page" +
-            ".js\"></script>"
-            + "<script type=\"text/javascript\">SyntaxHighlighter.all();</script>"
-            + "<script type=\"text/javascript\">function showImagePreview(var url){window" +
-            ".location.url= url;}</script>"
-            + "<link rel=\"stylesheet\" type=\"text/css\" " +
-            "href=\"file:///android_asset/shThemeDefault.css\">"
-            + "<link rel=\"stylesheet\" type=\"text/css\" href=\"file:///android_asset/shCore" +
-            ".css\">"
-            + "<link rel=\"stylesheet\" type=\"text/css\" href=\"file:///android_asset/css/common_new" +
-            ".css\">";
-
-    private String getWebViewBody(BlogDetail blog) {
-        return linkCss + UIHelper.WEB_LOAD_IMAGES +
-                ThemeSwitchUtils.getWebViewBodyString() +
-                UIHelper.setHtmlCotentSupportImagePreview(blog.getBody()) +
-                "</div></body>";
-    }
 
     private void handleData(BlogDetail blog) {
+        mEmptyLayout.setVisibility(View.INVISIBLE);
         mBlog = blog;
-        String body = getWebViewBody(blog);
-        if (mWebView != null) {
-            mWebView.loadDataWithBaseURL("", body, "text/html", "UTF-8", "");
-            // 显示存储的字体大小
-            mWebView.loadUrl(FontSizeUtils.getSaveFontSize());
-        }
-
-        mTVAuthorName.setText(blog.getAuthor());
-        mImgLoader.load(blog.getAuthorPortrait()).error(R.drawable.widget_dface).into(mIVAuthorPortrait);
-
-        mTVPubDate.setText(blog.getPubDate());
-        mTVTitle.setText(blog.getTitle());
-
-
-        mIVLabelRecommend.setVisibility(blog.isRecommend() ? View.VISIBLE : View.GONE);
-        mIVLabelOriginate.setImageDrawable(blog.isOriginal() ?
-                getResources().getDrawable(R.drawable.ic_label_originate) :
-                getResources().getDrawable(R.drawable.ic_label_reprint));
-
-        if (blog.getAuthorRelation() == 3) {
-            mBtnRelation.setEnabled(true);
-            mBtnRelation.setText("关注");
-            mBtnRelation.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    mBtnRelation.setEnabled(false);
-                    updateRelation();
-                }
-            });
-
-        } else {
-            mBtnRelation.setEnabled(false);
-            mBtnRelation.setText("已关注");
-        }
-
-        for (BlogDetail.About about : blog.getAbouts()) {
-            View aboutLay = getLayoutInflater().inflate(R.layout.item_blog_detail_about_lay, mLayAbouts, true);
-            ((TextView) aboutLay.findViewById(R.id.tv_title)).setText(about.title);
-            ((TextView) aboutLay.findViewById(R.id.tv_info_view)).setText(about.viewCount);
-            ((TextView) aboutLay.findViewById(R.id.tv_info_comment)).setText(about.commentCount);
-        }
-
-        mTVCommentCount.setText(String.format("评论(%s)", blog.getCommentCount()));
-        for (final BlogDetail.Comment comment : blog.getComments()) {
-            View aboutLay = getLayoutInflater().inflate(R.layout.item_blog_detail_comment_lay, mLayComments, true);
-            mImgLoader.load(comment.authorPortrait).error(R.drawable.widget_dface)
-                    .into(((ImageView) aboutLay.findViewById(R.id.iv_avatar)));
-
-            ((TextView) aboutLay.findViewById(R.id.tv_name)).setText(comment.author);
-            ((TextView) aboutLay.findViewById(R.id.tv_pub_date)).setText(comment.pubDate);
-            ((TextView) aboutLay.findViewById(R.id.tv_content)).setText(comment.content);
-            final long commentId = comment.id;
-            aboutLay.findViewById(R.id.btn_comment).setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    mCommentId = commentId;
-                    mETInput.setText(String.format("回复: %s", comment.author));
-                }
-            });
-        }
+        showBlog();
     }
 
-    private void submitComment(long id, String content) {
+
+    @Override
+    public void toFavorite() {
 
     }
 
     @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.iv_flow: {
+    public void toShare() {
 
-            }
-            break;
-            case R.id.iv_share: {
-
-            }
-            break;
-        }
     }
 
-
-    private void updateRelation() {
+    @Override
+    public void toFollow() {
         int mId = AppContext.getInstance().getLoginUid();
         OSChinaApi.updateRelation(mId, mBlog.getAuthorId(), 1,
                 new AsyncHttpResponseHandler() {
@@ -291,10 +159,8 @@ public class BlogDetailActivity extends AppCompatActivity implements View.OnClic
                             Result result = XmlUtils.toBean(net.oschina.app.bean.ResultBean.class,
                                     new ByteArrayInputStream(arg2)).getResult();
                             if (result.OK()) {
-                                mBtnRelation.setText("已关注");
-                                return;
+                                mView.toFollowOk();
                             }
-                            mBtnRelation.setEnabled(true);
                         } catch (Exception e) {
                             e.printStackTrace();
                             onFailure(arg0, arg1, arg2, e);
@@ -304,8 +170,13 @@ public class BlogDetailActivity extends AppCompatActivity implements View.OnClic
                     @Override
                     public void onFailure(int arg0, Header[] arg1, byte[] arg2,
                                           Throwable arg3) {
-                        mBtnRelation.setEnabled(true);
+
                     }
                 });
+    }
+
+    @Override
+    public void toSendComment(long id, String comment) {
+
     }
 }
