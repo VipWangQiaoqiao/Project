9795d7e55f1acbb1b547b791413698605e4525ed
Qiujuer <qiujuer@live.cn>
2016-06-20 17:32:43 +0800

    add proguard-rules

---
+++ app/src/main/java/net/oschina/app/improve/activities/detail/DetailActivity.java
@@ -0,0 +1,11 @@
+package net.oschina.app.improve.activities.detail;
+
+import net.oschina.app.improve.activities.BaseActivity;
+
+/**
+ * Created by JuQiu
+ * on 16/6/20.
+ */
+
+public abstract class DetailActivity extends BaseActivity {
+}
