5206b9dcc78fb0f9c60744f06bd17ddcfc465b76
Qiujuer <qiujuer@live.cn>
2016-06-22 17:30:38 +0800

    detail web view add show image

--- app/src/main/java/net/oschina/app/improve/utils/HtmlUtil.java
+++ app/src/main/java/net/oschina/app/improve/utils/HtmlUtil.java
@@ -1,7 +1,8 @@
 package net.oschina.app.improve.utils;
 
-import android.util.Log;
 import android.webkit.WebView;
+
+import net.oschina.app.util.HTMLUtil;
 
 /**
  * Created by JuQiu
@@ -24,36 +25,9 @@ public final class HtmlUtil {
 
     public static final String WEB_LOAD_IMAGES = "<script type=\"text/javascript\"> var allImgUrls = getAllImgSrc(document.body.innerHTML);</script>";
 
-
     public static final void initDetailView(WebView webView, String content) {
-        String body = String.format(HTML_FRAME, HtmlUtil.TITLE_DETAIL + HtmlUtil.WEB_LOAD_IMAGES, content);
-
-        /*
-        webView.setWebViewClient(new WebViewClient() {
-            @Override
-            public void onPageStarted(WebView view, String url, Bitmap favicon) {
-                super.onPageStarted(view, url, favicon);
-                showLog("WebViewClient.onPageStarted:" + url);
-            }
-
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                super.onPageFinished(view, url);
-                showLog("WebViewClient.onPageFinished:" + url);
-            }
-
-            @Override
-            public void onLoadResource(WebView view, String url) {
-                super.onLoadResource(view, url);
-                showLog("WebViewClient.onLoadResource:" + url);
-            }
-        });
-        */
-
+        //String body = String.format(HTML_FRAME, HtmlUtil.TITLE_DETAIL + HtmlUtil.WEB_LOAD_IMAGES, content);
+        String body = HTMLUtil.setupWebContent(content, true, true);
         webView.loadDataWithBaseURL("", body, "text/html", "UTF-8", "");
-    }
-
-    private static void showLog(String log) {
-        Log.e(HtmlUtil.class.getName(), log);
     }
 }
