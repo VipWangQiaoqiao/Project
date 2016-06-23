5956d5942e2c93347e42867dc4d8808824d0ef3e
Qiujuer <qiujuer@live.cn>
2016-06-23 17:23:21 +0800

    improve title comment logic

--- app/src/main/java/net/oschina/app/improve/detail/fragments/DetailFragment.java
+++ app/src/main/java/net/oschina/app/improve/detail/fragments/DetailFragment.java
@@ -2,6 +2,8 @@ package net.oschina.app.improve.detail.fragments;
 
 import android.content.Context;
 import android.support.annotation.IdRes;
+import android.support.v4.widget.NestedScrollView;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
@@ -19,6 +21,8 @@ import net.oschina.app.improve.utils.HtmlUtil;
 public abstract class DetailFragment<Data, DataView extends DetailContract.View, Operator extends DetailContract.Operator<Data, DataView>> extends BaseFragment implements DetailContract.View {
     protected Operator mOperator;
     protected WebView mWebView;
+    private NestedScrollView mScrollView;
+    private View mScrollTargetView;
 
     public Operator getOperator() {
         return mOperator;
@@ -63,6 +67,55 @@ public abstract class DetailFragment<Data, DataView extends DetailContract.View,
         }
     }
 
+    void registerScroller(NestedScrollView nestedScrollView, View target) {
+        mScrollView = nestedScrollView;
+        mScrollTargetView = target;
+    }
+
+    private int mScrollYPoint = -1;
+
+    @Override
+    public void scrollToComment() {
+        NestedScrollView nestedScrollView = mScrollView;
+        View target = mScrollTargetView;
+        if (nestedScrollView != null && target != null) {
+            int curY = nestedScrollView.getScrollY();
+            int targetY = target.getTop();
+            Log.e("TAG", "curY:" + curY + " targetY:" + targetY + " mScrollYPoint:" + mScrollYPoint);
+            if (targetY > 0) {
+                if (curY == targetY && targetY == mScrollYPoint) {
+                    nestedScrollView.fullScroll(View.FOCUS_UP);
+                } else {
+                    if (mScrollYPoint == -1) {
+                        nestedScrollView.smoothScrollTo(0, targetY);
+                        mScrollYPoint = curY;
+                        return;
+                    }
+                    if (curY > targetY) {
+                        // 当前在评论之后
+                        if (mScrollYPoint < targetY) {
+                            nestedScrollView.smoothScrollTo(0, mScrollYPoint);
+                        } else {
+                            nestedScrollView.fullScroll(View.FOCUS_UP);
+                        }
+                    } else {
+                        // 当前在评论之前
+                        nestedScrollView.smoothScrollTo(0, mScrollYPoint);
+                    }
+                    mScrollYPoint = curY;
+                }
+            } else {
+                if (mScrollYPoint == -1) {
+                    nestedScrollView.fullScroll(View.FOCUS_DOWN);
+                    mScrollYPoint = curY;
+                } else {
+                    nestedScrollView.smoothScrollTo(0, mScrollYPoint);
+                    mScrollYPoint = -1;
+                }
+            }
+        }
+    }
+
     @Override
     public void onResume() {
         super.onResume();
@@ -94,6 +147,14 @@ public abstract class DetailFragment<Data, DataView extends DetailContract.View,
             view.clearCache(true);
             view.destroy();
         }
+
+        mScrollTargetView = null;
+        NestedScrollView nestedScrollView = mScrollView;
+        if (nestedScrollView != null) {
+            mScrollView = null;
+            nestedScrollView.setOnScrollChangeListener((NestedScrollView.OnScrollChangeListener) null);
+        }
+
         mOperator = null;
 
         super.onDestroy();
