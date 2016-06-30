9c2e21c77647490d0a302c61a73f963fe2f16b42
uni7corn <yaoqi@oschina.cn>
2016-06-30 12:32:10 +0800

    fixed the redirect bug  for the user favarition module

--- app/src/main/java/net/oschina/app/fragment/UserFavoriteFragment.java
+++ app/src/main/java/net/oschina/app/fragment/UserFavoriteFragment.java
@@ -18,61 +18,62 @@ import java.io.Serializable;
 public class UserFavoriteFragment extends BaseListFragment<Favorite> {
 
     protected static final String TAG = UserFavoriteFragment.class
-	    .getSimpleName();
+            .getSimpleName();
     private static final String CACHE_KEY_PREFIX = "userfavorite_";
 
     @Override
     protected UserFavoriteAdapter getListAdapter() {
-	return new UserFavoriteAdapter();
+        return new UserFavoriteAdapter();
     }
 
     @Override
     protected String getCacheKeyPrefix() {
-	return CACHE_KEY_PREFIX + mCatalog;
+        return CACHE_KEY_PREFIX + mCatalog;
     }
 
     @Override
     protected FavoriteList parseList(InputStream is) throws Exception {
-		return XmlUtils.toBean(FavoriteList.class, is);
+        return XmlUtils.toBean(FavoriteList.class, is);
     }
 
     @Override
     protected FavoriteList readList(Serializable seri) {
-	return ((FavoriteList) seri);
+        return ((FavoriteList) seri);
     }
 
     @Override
     protected void sendRequestData() {
-	OSChinaApi.getFavoriteList(AppContext.getInstance().getLoginUid(),
-		mCatalog, mCurrentPage, mHandler);
+        OSChinaApi.getFavoriteList(AppContext.getInstance().getLoginUid(),
+                mCatalog, mCurrentPage, mHandler);
     }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position,
-	    long id) {
+                            long id) {
 
-	Favorite favorite = (Favorite) mAdapter.getItem(position);
-	if (favorite != null) {
-	    switch (favorite.getType()) {
+        Favorite favorite = mAdapter.getItem(position);
+        if (favorite != null) {
+            switch (favorite.getType()) {
 
-	    case Favorite.CATALOG_BLOGS:
-		UIHelper.showUrlRedirect(getActivity(), favorite.getUrl());
-		break;
-	    case Favorite.CATALOG_CODE:
-		UIHelper.showUrlRedirect(getActivity(), favorite.getUrl());
-		break;
-	    case Favorite.CATALOG_NEWS:
-		UIHelper.showUrlRedirect(getActivity(), favorite.getUrl());
-		break;
-	    case Favorite.CATALOG_SOFTWARE:
-		UIHelper.showUrlRedirect(getActivity(), favorite.getUrl());
-		break;
-	    case Favorite.CATALOG_TOPIC:
-		UIHelper.showUrlRedirect(getActivity(), favorite.getUrl());
-		break;
+                case Favorite.CATALOG_BLOGS:
+                    UIHelper.showUrlRedirect(getActivity(), favorite.getUrl());
+                    break;
+                case Favorite.CATALOG_CODE:
+                    UIHelper.showUrlRedirect(getActivity(), favorite.getUrl());
+                    break;
+                case Favorite.CATALOG_NEWS:
+                    UIHelper.showUrlRedirect(getActivity(), favorite.getUrl());
+                    break;
+                case Favorite.CATALOG_SOFTWARE:
+                    UIHelper.showDetail(getActivity(), 1, favorite.getId(), favorite.getUrl());
+                    //UIHelper.showUrlRedirect(getActivity(), favorite.getUrl());
+                    break;
+                case Favorite.CATALOG_TOPIC:
+                    UIHelper.showUrlRedirect(getActivity(), favorite.getUrl());
+                    break;
 
-	    }
-	}
+            }
+        }
 
     }
 }
