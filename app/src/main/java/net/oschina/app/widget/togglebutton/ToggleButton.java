7715d0e0ea2b1f3301be007f41100bcff9e3e398
Qiujuer <qiujuer@live.cn>
2016-07-01 18:02:07 +0800

    change the toggle button widget

--- app/src/main/java/net/oschina/app/widget/togglebutton/ToggleButton.java
+++ app/src/main/java/net/oschina/app/widget/togglebutton/ToggleButton.java
@@ -1,11 +1,7 @@
 package net.oschina.app.widget.togglebutton;
 
-import net.oschina.app.R;
-import net.oschina.app.widget.tooglebutton.rebound.SimpleSpringListener;
-import net.oschina.app.widget.tooglebutton.rebound.Spring;
-import net.oschina.app.widget.tooglebutton.rebound.SpringConfig;
-import net.oschina.app.widget.tooglebutton.rebound.SpringSystem;
-import net.oschina.app.widget.tooglebutton.rebound.SpringUtil;
+import android.animation.ObjectAnimator;
+import android.animation.TypeEvaluator;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
@@ -14,254 +10,325 @@ import android.graphics.Paint;
 import android.graphics.Paint.Cap;
 import android.graphics.Paint.Style;
 import android.graphics.RectF;
+import android.os.Build;
 import android.util.AttributeSet;
+import android.util.Property;
 import android.view.View;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.Interpolator;
+
+import net.oschina.app.R;
 
 /**
- * @author ThinkPad
- * 
+ * @author Qiujuer
  */
 public class ToggleButton extends View {
-	private SpringSystem springSystem;
-	private Spring spring ;
-	/** */
-	private float radius;
-	/** 开启颜色*/
-	private int onColor = Color.parseColor("#4ebb7f");
-	/** 关闭颜色*/
-	private int offBorderColor = Color.parseColor("#dadbda");
-	/** 灰色带颜色*/
-	private int offColor = Color.parseColor("#ffffff");
-	/** 手柄颜色*/
-	private int spotColor = Color.parseColor("#ffffff");
-	/** 边框颜色*/
-	private int borderColor = offBorderColor;
-	/** 画笔*/
-	private Paint paint ;
-	/** 开关状态*/
-	private boolean toggleOn = false;
-	/** 边框大小*/
-	private int borderWidth = 2;
-	/** 垂直中心*/
-	private float centerY;
-	/** 按钮的开始和结束位置*/
-	private float startX, endX;
-	/** 手柄X位置的最小和最大值*/
-	private float spotMinX, spotMaxX;
-	/**手柄大小 */
-	private int spotSize ;
-	/** 手柄X位置*/
-	private float spotX;
-	/** 关闭时内部灰色带高度*/
-	private float offLineWidth;
-	/** */
-	private RectF rect = new RectF();
-	
-	private OnToggleChanged listener;
-	
-	private ToggleButton(Context context) {
-		super(context);
-	}
-	public ToggleButton(Context context, AttributeSet attrs, int defStyleAttr) {
-		super(context, attrs, defStyleAttr);
-		setup(attrs);
-	}
-	public ToggleButton(Context context, AttributeSet attrs) {
-		super(context, attrs);
-		setup(attrs);
-	}
-	
-	@Override
-	protected void onDetachedFromWindow() {
-		super.onDetachedFromWindow();
-		spring.removeListener(springListener);
-	}
-	
-	public void onAttachedToWindow() {
-		super.onAttachedToWindow();
-		spring.addListener(springListener);
-	}
+    /** */
+    private float radius;
+    /**
+     * 开启颜色
+     */
+    private int onColor = Color.parseColor("#4ebb7f");
+    /**
+     * 关闭颜色
+     */
+    private int offBorderColor = Color.parseColor("#dadbda");
+    /**
+     * 灰色带颜色
+     */
+    private int offColor = Color.parseColor("#ffffff");
+    /**
+     * 手柄颜色
+     */
+    private int spotColor = Color.parseColor("#ffffff");
+    /**
+     * 边框颜色
+     */
+    private int borderColor = offBorderColor;
+    /**
+     * 画笔
+     */
+    private Paint paint;
+    /**
+     * 开关状态
+     */
+    private boolean toggleOn = false;
+    /**
+     * 边框大小
+     */
+    private int borderWidth = 2;
+    /**
+     * 垂直中心
+     */
+    private float centerY;
+    /**
+     * 按钮的开始和结束位置
+     */
+    private float startX, endX;
+    /**
+     * 手柄X位置的最小和最大值
+     */
+    private float spotMinX, spotMaxX;
+    /**
+     * 手柄大小
+     */
+    private int spotSize;
+    /**
+     * 手柄X位置
+     */
+    private float spotX;
+    /**
+     * 关闭时内部灰色带高度
+     */
+    private float offLineWidth;
+    /** */
+    private RectF rect = new RectF();
 
-	public void setup(AttributeSet attrs) {
-		paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-		paint.setStyle(Style.FILL);
-		paint.setStrokeCap(Cap.ROUND);
-		
-		springSystem = SpringSystem.create();
-		spring = springSystem.createSpring();
-		spring.setSpringConfig(SpringConfig.fromOrigamiTensionAndFriction(50, 7));
-		
-		this.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View arg0) {
-				toggle();
-			}
-		});
-		
-		TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.ToggleButton);
-		offBorderColor = typedArray.getColor(R.styleable.ToggleButton_offBorderColor, offBorderColor);
-		onColor = typedArray.getColor(R.styleable.ToggleButton_onColor, onColor);
-		spotColor = typedArray.getColor(R.styleable.ToggleButton_spotColor, spotColor);
-		offColor = typedArray.getColor(R.styleable.ToggleButton_offColor, offColor);
-		borderWidth = typedArray.getDimensionPixelSize(R.styleable.ToggleButton_toggle_border_width, borderWidth);
-		typedArray.recycle();
-	}
-	
-	public void toggle() {
-		toggleOn = !toggleOn;
-		spring.setEndValue(toggleOn ? 1 : 0);
-		if(listener != null){
-			listener.onToggle(toggleOn);
-		}
-	}
-	
-	public void toggleOn() {
-		setToggleOn();
-		if(listener != null){
-			listener.onToggle(toggleOn);
-		}
-	}
-	
-	public void toggleOff() {
-		setToggleOff();
-		if(listener != null){
-			listener.onToggle(toggleOn);
-		}
-	}
-	
-	/**
-	 * 设置显示成打开样式，不会触发toggle事件
-	 */
-	public void setToggleOn() {
-		toggleOn = true;
-		spring.setEndValue(toggleOn ? 1 : 0);
-	}
-	
-	/**
-	 * 设置显示成关闭样式，不会触发toggle事件
-	 */
-	public void setToggleOff() {
-		toggleOn = false;
-		spring.setEndValue(toggleOn ? 1 : 0);
-	}
-	
-	@Override
-	protected void onLayout(boolean changed, int left, int top, int right,
-			int bottom) {
-		super.onLayout(changed, left, top, right, bottom);
-		
-		final int width = getWidth();
-		final int height = getHeight();
-		radius = Math.min(width, height) * 0.5f;
-		centerY = radius;
-		startX = radius;
-		endX = width - radius;
-		spotMinX = startX + borderWidth;
-		spotMaxX = endX - borderWidth;
-		spotSize = height - 4 * borderWidth;
-		spotX = spotMinX;
-		offLineWidth = 0;
-	}
-	
-	
-	SimpleSpringListener springListener = new SimpleSpringListener(){
-		@Override
-		public void onSpringUpdate(Spring spring) {
-			final double value = spring.getCurrentValue();
-			
-			final float mapToggleX = (float) SpringUtil.mapValueFromRangeToRange(value, 0, 1, spotMinX, spotMaxX);
-			spotX = mapToggleX;
-			
-			float mapOffLineWidth = (float) SpringUtil.mapValueFromRangeToRange(1 - value, 0, 1, 10, spotSize);
-			
-			offLineWidth = mapOffLineWidth;
-			
-			final int fb = Color.blue(onColor);
-			final int fr = Color.red(onColor);
-			final int fg = Color.green(onColor);
-			
-			final int tb = Color.blue(offBorderColor);
-			final int tr = Color.red(offBorderColor);
-			final int tg = Color.green(offBorderColor);
-			
-			int sb = (int) SpringUtil.mapValueFromRangeToRange(1 - value, 0, 1, fb, tb);
-			int sr = (int) SpringUtil.mapValueFromRangeToRange(1 - value, 0, 1, fr, tr);
-			int sg = (int) SpringUtil.mapValueFromRangeToRange(1 - value, 0, 1, fg, tg);
-			
-			sb = SpringUtil.clamp(sb, 0, 255);
-			sr = SpringUtil.clamp(sr, 0, 255);
-			sg = SpringUtil.clamp(sg, 0, 255);
-			
-			borderColor = Color.rgb(sr, sg, sb);
-			
-			postInvalidate();
-		}
-	};
-	
-	
-	@Override
-	public void draw(Canvas canvas) {
-		
-		/*
-		final int height = getHeight();
-		//绘制背景（边框）
-		paint.setStrokeWidth(height);
-		paint.setColor(borderColor);
-		canvas.drawLine(startX, centerY, endX, centerY, paint);
-		//绘制灰色带
-		if(offLineWidth > 0){
-			paint.setStrokeWidth(offLineWidth);
-			paint.setColor(offColor);
-			canvas.drawLine(spotX, centerY, endX, centerY, paint);
-		}
-		//spot的边框
-		paint.setStrokeWidth(height);
-		paint.setColor(borderColor);
-		canvas.drawLine(spotX - 1, centerY, spotX + 1.1f, centerY, paint);
-		//spot
-		paint.setStrokeWidth(spotSize);
-		paint.setColor(spotColor);
-		canvas.drawLine(spotX, centerY, spotX + 0.1f, centerY, paint);
-		*/
-		
-		
-		//
-		rect.set(0, 0, getWidth(), getHeight());
-		paint.setColor(borderColor);
-		canvas.drawRoundRect(rect, radius, radius, paint);
-		
-		if(offLineWidth > 0){
-			final float cy = offLineWidth * 0.5f;
-			rect.set(spotX - cy, centerY - cy, endX + cy, centerY + cy);
-			paint.setColor(offColor);
-			canvas.drawRoundRect(rect, cy, cy, paint);
-		}
-		
-		rect.set(spotX - 1 - radius, centerY - radius, spotX + 1.1f + radius, centerY + radius);
-		paint.setColor(borderColor);
-		canvas.drawRoundRect(rect, radius, radius, paint);
-		
-		final float spotR = spotSize * 0.5f;
-		rect.set(spotX - spotR, centerY - spotR, spotX + spotR, centerY + spotR);
-		paint.setColor(spotColor);
-		canvas.drawRoundRect(rect, spotR, spotR, paint);
-		
-	}
-	
-	
-	/**
-	 * @author ThinkPad
-	 *
-	 */
-	public interface OnToggleChanged{
-		/**
-		 * @param on
-		 */
-		public void onToggle(boolean on);
-	}
+    private OnToggleChanged listener;
+
+    private ToggleButton(Context context) {
+        super(context);
+    }
+
+    public ToggleButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        setup(attrs);
+    }
+
+    public ToggleButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setup(attrs);
+    }
+
+    public void setup(AttributeSet attrs) {
+        paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setStyle(Style.FILL);
+        paint.setStrokeCap(Cap.ROUND);
+
+        this.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                toggle();
+            }
+        });
+
+        TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.ToggleButton);
+        offBorderColor = typedArray.getColor(R.styleable.ToggleButton_offBorderColor, offBorderColor);
+        onColor = typedArray.getColor(R.styleable.ToggleButton_onColor, onColor);
+        spotColor = typedArray.getColor(R.styleable.ToggleButton_spotColor, spotColor);
+        offColor = typedArray.getColor(R.styleable.ToggleButton_offColor, offColor);
+        borderWidth = typedArray.getDimensionPixelSize(R.styleable.ToggleButton_toggle_border_width, borderWidth);
+        typedArray.recycle();
+    }
+
+    public void toggle() {
+        toggleOn = !toggleOn;
+        animateCheckedState(toggleOn);
+        if (listener != null) {
+            listener.onToggle(toggleOn);
+        }
+    }
+
+    public void toggleOn() {
+        setToggleOn();
+        if (listener != null) {
+            listener.onToggle(toggleOn);
+        }
+    }
+
+    public void toggleOff() {
+        setToggleOff();
+        if (listener != null) {
+            listener.onToggle(toggleOn);
+        }
+    }
+
+    /**
+     * 设置显示成打开样式，不会触发toggle事件
+     */
+    public void setToggleOn() {
+        toggleOn = true;
+        setAnimatorProperty(true);
+    }
+
+    /**
+     * 设置显示成关闭样式，不会触发toggle事件
+     */
+    public void setToggleOff() {
+        toggleOn = false;
+        setAnimatorProperty(false);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right,
+                            int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+
+        final int width = getWidth();
+        final int height = getHeight();
+        radius = Math.min(width, height) * 0.5f;
+        centerY = radius;
+        startX = radius;
+        endX = width - radius;
+        spotMinX = startX + borderWidth;
+        spotMaxX = endX - borderWidth;
+        spotSize = height - 4 * borderWidth;
+
+        // update values
+        setAnimatorProperty(toggleOn);
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        rect.set(0, 0, getWidth(), getHeight());
+        paint.setColor(borderColor);
+        canvas.drawRoundRect(rect, radius, radius, paint);
+
+        if (offLineWidth > 0) {
+            final float cy = offLineWidth * 0.5f;
+            rect.set(spotX - cy, centerY - cy, endX + cy, centerY + cy);
+            paint.setColor(offColor);
+            canvas.drawRoundRect(rect, cy, cy, paint);
+        }
+
+        rect.set(spotX - 1 - radius, centerY - radius, spotX + 1.1f + radius, centerY + radius);
+        paint.setColor(borderColor);
+        canvas.drawRoundRect(rect, radius, radius, paint);
+
+        final float spotR = spotSize * 0.5f;
+        rect.set(spotX - spotR, centerY - spotR, spotX + spotR, centerY + spotR);
+        paint.setColor(spotColor);
+        canvas.drawRoundRect(rect, spotR, spotR, paint);
+    }
+
+    /**
+     * =============================================================================================
+     * The Animate
+     * =============================================================================================
+     */
+    private static final Interpolator ANIMATION_INTERPOLATOR = new DecelerateInterpolator();
+    private static final int ANIMATION_DURATION = 280;
+    private ObjectAnimator mAnimator;
+
+    private void animateCheckedState(boolean newCheckedState) {
+        AnimatorProperty property = new AnimatorProperty();
+        if (newCheckedState) {
+            property.color = onColor;
+            property.offLineWidth = 10;
+            property.spotX = spotMaxX;
+        } else {
+            property.color = offBorderColor;
+            property.offLineWidth = spotSize;
+            property.spotX = spotMinX;
+        }
+
+        if (mAnimator == null) {
+            mAnimator = ObjectAnimator.ofObject(this, ANIM_VALUE, new AnimatorEvaluator(mCurProperty), property);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2)
+                mAnimator.setAutoCancel(true);
+            mAnimator.setDuration(ANIMATION_DURATION);
+            mAnimator.setInterpolator(ANIMATION_INTERPOLATOR);
+        } else {
+            mAnimator.cancel();
+            mAnimator.setObjectValues(property);
+        }
+        mAnimator.start();
+    }
+
+    /**
+     * =============================================================================================
+     * The custom properties
+     * =============================================================================================
+     */
+
+    private AnimatorProperty mCurProperty = new AnimatorProperty();
+
+    private void setAnimatorProperty(AnimatorProperty property) {
+        this.spotX = property.spotX;
+        this.borderColor = property.color;
+        this.offLineWidth = property.offLineWidth;
+        invalidate();
+    }
+
+    private void setAnimatorProperty(boolean isOn) {
+        AnimatorProperty property = mCurProperty;
+        if (isOn) {
+            property.color = onColor;
+            property.offLineWidth = 10;
+            property.spotX = spotMaxX;
+        } else {
+            property.color = offBorderColor;
+            property.offLineWidth = spotSize;
+            property.spotX = spotMinX;
+        }
+        setAnimatorProperty(property);
+    }
+
+    private final static class AnimatorProperty {
+        private int color;
+        private float offLineWidth;
+        private float spotX;
+    }
+
+    private final static class AnimatorEvaluator implements TypeEvaluator<AnimatorProperty> {
+        private final AnimatorProperty mProperty;
+
+        public AnimatorEvaluator(AnimatorProperty property) {
+            mProperty = property;
+        }
+
+        @Override
+        public AnimatorProperty evaluate(float fraction, AnimatorProperty startValue, AnimatorProperty endValue) {
+            // Values
+            mProperty.spotX = (int) (startValue.spotX + (endValue.spotX - startValue.spotX) * fraction);
+
+            mProperty.offLineWidth = (int) (startValue.offLineWidth + (endValue.offLineWidth - startValue.offLineWidth) * (1 - fraction));
+
+            // Color
+            int startA = (startValue.color >> 24) & 0xff;
+            int startR = (startValue.color >> 16) & 0xff;
+            int startG = (startValue.color >> 8) & 0xff;
+            int startB = startValue.color & 0xff;
+
+            int endA = (endValue.color >> 24) & 0xff;
+            int endR = (endValue.color >> 16) & 0xff;
+            int endG = (endValue.color >> 8) & 0xff;
+            int endB = endValue.color & 0xff;
+
+            mProperty.color = (startA + (int) (fraction * (endA - startA))) << 24 |
+                    (startR + (int) (fraction * (endR - startR))) << 16 |
+                    (startG + (int) (fraction * (endG - startG))) << 8 |
+                    (startB + (int) (fraction * (endB - startB)));
+
+            return mProperty;
+        }
+    }
+
+    private final static Property<ToggleButton, AnimatorProperty> ANIM_VALUE = new Property<ToggleButton, AnimatorProperty>(AnimatorProperty.class, "animValue") {
+        @Override
+        public AnimatorProperty get(ToggleButton object) {
+            return object.mCurProperty;
+        }
+
+        @Override
+        public void set(ToggleButton object, AnimatorProperty value) {
+            object.setAnimatorProperty(value);
+        }
+    };
 
 
-	public void setOnToggleChanged(OnToggleChanged onToggleChanged) {
-		listener = onToggleChanged;
-	}
+    /**
+     * @author ThinkPad
+     */
+    public interface OnToggleChanged {
+        /**
+         * @param on
+         */
+        public void onToggle(boolean on);
+    }
+
+
+    public void setOnToggleChanged(OnToggleChanged onToggleChanged) {
+        listener = onToggleChanged;
+    }
 }
