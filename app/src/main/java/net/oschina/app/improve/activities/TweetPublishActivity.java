a9621e0a59d0059d76a925e59cb2fb2b56682e58
Qiujuer <qiujuer@live.cn>
2016-07-14 11:25:30 +0800

    add tweet publish activity

---
+++ app/src/main/java/net/oschina/app/improve/activities/TweetPublishActivity.java
@@ -0,0 +1,10 @@
+package net.oschina.app.improve.activities;
+
+import net.oschina.app.R;
+
+public class TweetPublishActivity extends BaseBackActivity {
+    @Override
+    protected int getContentView() {
+        return R.layout.activity_tweet_publish;
+    }
+}
