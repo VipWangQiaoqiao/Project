e828994e19854efd323464a46e7735b3bd42fd90
Qiujuer <qiujuer@live.cn>
2016-07-19 14:14:28 +0800

    change the tweet publish logic

--- app/src/main/java/net/oschina/app/improve/tweet/service/TweetPublishService.java
+++ app/src/main/java/net/oschina/app/improve/tweet/service/TweetPublishService.java
@@ -4,15 +4,22 @@ import android.app.IntentService;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import net.oschina.app.api.remote.OSChinaApi;
 import net.oschina.app.improve.app.AppOperator;
+import net.oschina.app.improve.bean.base.ResultBean;
+import net.oschina.app.improve.bean.resource.ImageResource;
 
 import org.kymjs.kjframe.bitmap.BitmapCreate;
 import org.kymjs.kjframe.utils.FileUtils;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,6 +30,7 @@ import cz.msebera.android.httpclient.Header;
  * 专用于动弹发布
  */
 public class TweetPublishService extends IntentService {
+    private final static String TAG = TweetPublishService.class.getName();
     // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
     private static final String ACTION_PUBLISH = "net.oschina.app.improve.tweet.service.action.PUBLISH";
 
@@ -70,6 +78,8 @@ public class TweetPublishService extends IntentService {
     private void handleActionPublish(String content, String[] images) {
         if (images == null) {
             publish(content, null);
+        } else {
+            uploadImageAndPublish(content, images);
         }
     }
 
@@ -78,24 +88,25 @@ public class TweetPublishService extends IntentService {
         OSChinaApi.pubTweet(content, imageToken, null, new LopperResponseHandler() {
             @Override
             public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
-                //Toast.makeText(TweetPublishActivity.this, "发送动弹失败~", Toast.LENGTH_SHORT).show();
+                log("发送动弹失败~");
             }
 
             @Override
             public void onSuccess(int statusCode, Header[] headers, String responseString) {
-                //Toast.makeText(TweetPublishActivity.this, "发送动弹成功~", Toast.LENGTH_SHORT).show();
-
+                log("发送动弹成功~");
             }
         });
     }
 
-    private void uploadImageAndPublish(final String content, final List<String> paths) {
+    private void uploadImageAndPublish(final String content, final String[] paths) {
         AppOperator.runOnThread(new Runnable() {
             @Override
             public void run() {
-                List<String> uploadPaths = saveImageToCache(paths);
-                if (paths == null || paths.size() <= 0)
+                String[] uploadPaths = saveImageToCache(paths);
+                if (uploadPaths == null) {
+                    log("图片压缩存储失败~");
                     return;
+                }
                 uploadImages(0, null, uploadPaths, new UploadImageCallback() {
                     @Override
                     public void onDone(String token) {
@@ -110,26 +121,41 @@ public class TweetPublishService extends IntentService {
         void onDone(String token);
     }
 
-    private void uploadImages(final int index, final String token, final List<String> paths, final UploadImageCallback runnable) {
-        if (index < 0 || index >= paths.size())
+    private void uploadImages(final int index, final String token, final String[] paths, final UploadImageCallback runnable) {
+        if (index < 0 || index >= paths.length) {
             runnable.onDone(token);
+            return;
+        }
+        String path = paths[index];
 
-        OSChinaApi.uploadImage(token, paths.get(index), new LopperResponseHandler() {
+        log("发送图片: token:" + token + " path:" + path);
+
+        OSChinaApi.uploadImage(token, path, new LopperResponseHandler() {
 
             @Override
             public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
 
+                uploadImages(index, token, paths, runnable);
             }
 
             @Override
             public void onSuccess(int statusCode, Header[] headers, String responseString) {
-
+                Type type = new TypeToken<ResultBean<ImageResource>>() {
+                }.getType();
+                ResultBean<ImageResource> resultBean = new Gson().fromJson(responseString, type);
+                if (resultBean.isSuccess()) {
+                    String token = resultBean.getResult().getToken();
+                    log("发送图片成功~" + token);
+                    uploadImages(index + 1, token, paths, runnable);
+                } else {
+                    log("发送图片失败~ " + resultBean.getCode() + " " + resultBean.getMessage());
+                }
             }
         });
 
     }
 
-    private List<String> saveImageToCache(List<String> paths) {
+    private String[] saveImageToCache(String[] paths) {
         List<String> ret = new ArrayList<>();
         final String sdCardPath = FileUtils.getSDCardPath();
         for (String path : paths) {
@@ -144,6 +170,15 @@ public class TweetPublishService extends IntentService {
                 e.printStackTrace();
             }
         }
-        return ret;
+        if (ret.size() > 0) {
+            String[] images = new String[ret.size()];
+            ret.toArray(images);
+            return images;
+        }
+        return null;
+    }
+
+    private static void log(String str) {
+        Log.e(TAG, str);
     }
 }
