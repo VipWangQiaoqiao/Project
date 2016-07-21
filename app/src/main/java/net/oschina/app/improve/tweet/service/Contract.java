6df1508ba0d8474eb679c2ddf7f3eee1ed3d28b8
Qiujuer <qiujuer@live.cn>
2016-07-21 18:06:06 +0800

    improve the tweet publish service logic

--- app/src/main/java/net/oschina/app/improve/tweet/service/Contract.java
+++ app/src/main/java/net/oschina/app/improve/tweet/service/Contract.java
@@ -5,12 +5,20 @@ package net.oschina.app.improve.tweet.service;
  * on 16/7/21.
  */
 
-public interface Contract {
-    interface IService{
+interface Contract {
+    interface IService {
+        String getCachePath(String id);
 
+        void start(String modelId, IOperator operator);
+
+        void stop(String id, int startId);
+
+        void notifyMsg(int notifyId, String modelId, boolean haveReDo, boolean haveDelete, int resId, Object... values);
+
+        void notifyCancel(int notifyId);
     }
 
-    interface IOperator{
-
+    interface IOperator extends Runnable {
+        void stop();
     }
 }
--- app/src/main/java/net/oschina/app/improve/tweet/service/TweetPublishModel.java
+++ app/src/main/java/net/oschina/app/improve/tweet/service/TweetPublishModel.java
@@ -1,17 +1,70 @@
 package net.oschina.app.improve.tweet.service;
 
 import java.io.Serializable;
+import java.util.UUID;
 
 /**
  * Created by JuQiu
  * on 16/7/21.
  */
 
-public class TweetPublishModel implements Serializable {
+@SuppressWarnings("WeakerAccess")
+class TweetPublishModel implements Serializable {
     private String id;
     private String content;
-    private String[] images;
-    private String imageToken;
-    private int imageIndex;
+    private String[] srcImages;
+    private String[] cacheImages;
+    private String cacheImagesToken;
+    private int cacheImagesIndex;
 
+    public TweetPublishModel() {
+        id = UUID.randomUUID().toString();
+    }
+
+    public TweetPublishModel(String content, String[] images) {
+        this();
+        this.content = content;
+        this.srcImages = images;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String[] getSrcImages() {
+        return srcImages;
+    }
+
+    public String[] getCacheImages() {
+        return cacheImages;
+    }
+
+    public String getCacheImagesToken() {
+        return cacheImagesToken;
+    }
+
+    public int getCacheImagesIndex() {
+        return cacheImagesIndex;
+    }
+
+    public void setCacheImages(String[] cacheImages) {
+        this.cacheImages = cacheImages;
+        this.srcImages = null;
+    }
+
+    public void setCacheImagesToken(String cacheImagesToken) {
+        this.cacheImagesToken = cacheImagesToken;
+    }
+
+    public void setCacheImagesIndex(int cacheImagesIndex) {
+        this.cacheImagesIndex = cacheImagesIndex;
+    }
+
+    public void remove() {
+
+    }
 }
