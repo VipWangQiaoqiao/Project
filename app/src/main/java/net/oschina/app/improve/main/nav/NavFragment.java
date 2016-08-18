b04d415b5e7db3b878b885b2214f657934343187
Qiujuer <qiujuer@live.cn>
2016-08-18 10:34:18 +0800

    add navfragment

---
+++ app/src/main/java/net/oschina/app/improve/main/nav/NavFragment.java
@@ -0,0 +1,26 @@
+package net.oschina.app.improve.main.nav;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import net.oschina.app.R;
+import net.oschina.app.improve.base.fragments.BaseFragment;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class NavFragment extends BaseFragment {
+    public NavFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_nav;
+    }
+
+}
