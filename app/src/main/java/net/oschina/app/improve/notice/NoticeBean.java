7063510b085089faae76cd42e2e9bc585a6e681a
Qiujuer <qiujuer@live.cn>
2016-08-19 18:37:54 +0800

    done new NoticeServer and Manager

--- app/src/main/java/net/oschina/app/improve/notice/NoticeBean.java
+++ app/src/main/java/net/oschina/app/improve/notice/NoticeBean.java
@@ -1,10 +1,15 @@
 package net.oschina.app.improve.notice;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v4.content.SharedPreferencesCompat;
+
+import java.io.Serializable;
+
 /**
  * Created by JuQiu on 16/8/19.
  */
-
-public class NoticeBean {
+public class NoticeBean implements Serializable {
     private int mention;
     private int letter;
     private int review;
@@ -64,5 +69,36 @@ public class NoticeBean {
                 ", fans=" + fans +
                 ", like=" + like +
                 '}';
+    }
+
+    public NoticeBean add(NoticeBean bean) {
+        this.mention += bean.mention;
+        this.letter += bean.letter;
+        this.review += bean.review;
+        this.fans += bean.fans;
+        this.like += bean.like;
+        return this;
+    }
+
+    public static NoticeBean getInstance(Context context) {
+        SharedPreferences preferences = context.getSharedPreferences(NoticeBean.class.getName(), Context.MODE_PRIVATE);
+        NoticeBean bean = new NoticeBean();
+        bean.mention = preferences.getInt("mention", 0);
+        bean.letter = preferences.getInt("letter", 0);
+        bean.review = preferences.getInt("review", 0);
+        bean.fans = preferences.getInt("fans", 0);
+        bean.like = preferences.getInt("like", 0);
+        return bean;
+    }
+
+    public NoticeBean save(Context context) {
+        SharedPreferences.Editor editor = context.getSharedPreferences(NoticeBean.class.getName(), Context.MODE_PRIVATE).edit();
+        editor.putInt("mention", mention);
+        editor.putInt("letter", letter);
+        editor.putInt("review", review);
+        editor.putInt("fans", fans);
+        editor.putInt("like", like);
+        SharedPreferencesCompat.EditorCompat.getInstance().apply(editor);
+        return this;
     }
 }
