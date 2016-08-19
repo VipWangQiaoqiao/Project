72a0f774f96b07741f085ec4613045b96f2f677b
huanghaibin_dev <huanghaibin_dev@163.com>
2016-08-19 12:30:51 +0800

    update banner view and user message adapter

--- app/src/main/java/net/oschina/app/api/remote/OSChinaApi.java
+++ app/src/main/java/net/oschina/app/api/remote/OSChinaApi.java
@@ -1625,7 +1625,6 @@ public class OSChinaApi {
     }
 
     /**
-<<<<<<< Updated upstream
      * 获取消息列表
      *
      * @param authorId  authorId 用户id，不加该参数时返回所有给我发送消息的列表
@@ -1634,11 +1633,23 @@ public class OSChinaApi {
      */
     public static void getMessageList(long authorId, String pageToken, TextHttpResponseHandler handler) {
         RequestParams params = new RequestParams();
-        if (authorId != 0)
-            params.put("authorId", authorId);
+        params.put("authorId", authorId);
         if (!TextUtils.isEmpty(pageToken))
             params.put("pageToken", pageToken);
         ApiHttpClient.get("action/apiv2/messages", params, handler);
+    }
+
+    /**
+     * 获取用户私信列表
+     *
+     * @param pageToken pageToken
+     * @param handler   回调
+     */
+    public static void getUserMessageList(String pageToken, TextHttpResponseHandler handler) {
+        RequestParams params = new RequestParams();
+        if (!TextUtils.isEmpty(pageToken))
+            params.put("pageToken", pageToken);
+        ApiHttpClient.get("action/apiv2/user_msg_letters", params, handler);
     }
 
     /**
@@ -1683,10 +1694,11 @@ public class OSChinaApi {
 
     /**
      * 获取某用户的信息
-     * @param uid user id
+     *
+     * @param uid  user id
      * @param nick unique personal suffix
      */
-    public static void getUserInfo(Long uid, String nick, TextHttpResponseHandler handler){
+    public static void getUserInfo(Long uid, String nick, TextHttpResponseHandler handler) {
         RequestParams params = new RequestParams();
         params.put("id", uid);
         params.put("name", nick);
@@ -1695,11 +1707,12 @@ public class OSChinaApi {
 
     /**
      * 获取某用户的动态(讨论)列表
-     * @param uid user id
+     *
+     * @param uid       user id
      * @param pageToken page token
-     * @param handler async handler
+     * @param handler   async handler
      */
-    public static void getUserActives(Long uid, String pageToken, TextHttpResponseHandler handler){
+    public static void getUserActives(Long uid, String pageToken, TextHttpResponseHandler handler) {
         RequestParams params = new RequestParams();
         params.put("id", uid);
         if (!TextUtils.isEmpty(pageToken))
--- app/src/main/java/net/oschina/app/improve/user/fragments/UserMessageFragment.java
+++ app/src/main/java/net/oschina/app/improve/user/fragments/UserMessageFragment.java
@@ -1,7 +1,5 @@
 package net.oschina.app.improve.user.fragments;
 
-import android.view.View;
-
 import com.google.gson.reflect.TypeToken;
 
 import net.oschina.app.AppContext;
@@ -11,9 +9,8 @@ import net.oschina.app.improve.base.fragments.BaseRecyclerViewFragment;
 import net.oschina.app.improve.bean.Message;
 import net.oschina.app.improve.bean.base.PageBean;
 import net.oschina.app.improve.bean.base.ResultBean;
-import net.oschina.app.improve.bean.simple.Author;
+import net.oschina.app.improve.user.activities.UserSendMessageActivity;
 import net.oschina.app.improve.user.adapter.UserMessageAdapter;
-import net.oschina.app.ui.empty.EmptyLayout;
 
 import java.lang.reflect.Type;
 
@@ -34,29 +31,18 @@ public class UserMessageFragment extends BaseRecyclerViewFragment<Message> {
     @Override
     protected void requestData() {
         super.requestData();
-        OSChinaApi.getMessageList(authorId, mIsRefresh ? null : mBean.getNextPageToken(), mHandler);
+        OSChinaApi.getUserMessageList(mIsRefresh ? null : mBean.getNextPageToken(), mHandler);
+    }
+
+    @Override
+    public void onItemClick(int position, long itemId) {
+        Message message = mAdapter.getItem(position);
+        UserSendMessageActivity.show(getContext(), message.getSender().getId());
     }
 
     @Override
     protected BaseRecyclerAdapter<Message> getRecyclerAdapter() {
         return new UserMessageAdapter(this);
-    }
-
-    @Override
-    protected void onRequestError(int code) {
-        for (int i = 0; i < 20; i++) {
-            Message message = new Message();
-            message.setId(20);
-            message.setContent("今天开会啦");
-            message.setPubDate("2016-08-18 14:03:50");
-            message.setSender(new Author());
-            message.getSender().setName("天神哥");
-            message.getSender().setPortrait("https://static.oschina.net/uploads/user/1222/2445220_100.jpg?t=1468425830000");
-
-            mAdapter.addItem(message);
-        }mErrorLayout.setErrorType(EmptyLayout.HIDE_LAYOUT);
-        mRefreshLayout.setVisibility(View.VISIBLE);
-        super.onRequestError(code);
     }
 
     @Override
