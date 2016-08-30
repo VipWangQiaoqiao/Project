904aea1c5367844ee704a309cfada4b1473c2bf0
huanghaibin_dev <huanghaibin_dev@163.com>
2016-08-30 10:53:16 +0800

    add BaseGeneralRecyclerFragment and fix item bug

---
+++ app/src/main/java/net/oschina/app/improve/base/fragments/BaseGeneralRecyclerFragment.java
@@ -0,0 +1,18 @@
+package net.oschina.app.improve.base.fragments;
+
+import net.oschina.app.interf.OnTabReselectListener;
+
+/**
+ * Created by huanghaibin_dev
+ * on 2016/8/30.
+ */
+public abstract class BaseGeneralRecyclerFragment<T> extends BaseRecyclerViewFragment<T> implements OnTabReselectListener{
+    @Override
+    public void onTabReselect() {
+        if(mRecyclerView != null){
+            mRecyclerView.scrollToPosition(0);
+            mRefreshLayout.setRefreshing(true);
+            onRefreshing();
+        }
+    }
+}
