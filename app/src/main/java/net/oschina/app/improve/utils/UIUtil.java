150cd41a2014b896d5ab7609d31e2e96795fcdd1
Qiujuer <qiujuer@live.cn>
2016-09-05 11:48:14 +0800

    add titlebar and remove some UIUtil method

--- app/src/main/java/net/oschina/app/improve/utils/UIUtil.java
+++ app/src/main/java/net/oschina/app/improve/utils/UIUtil.java
@@ -17,66 +17,6 @@ public class UIUtil {
      * @return statusHeight
      */
     public static int getStatusHeight(Context context) {
-        int statusHeight = 0;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            try {
-                Class<?> clazz = Class.forName("com.android.internal.R$dimen");
-                Object object = clazz.newInstance();
-                int height = Integer.parseInt(clazz.getField("status_bar_height")
-                        .get(object).toString());
-                statusHeight = context.getResources().getDimensionPixelSize(height);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        return statusHeight;
-    }
-
-    /**
-     * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
-     */
-    public static int dipTopx(Context context, float dpValue) {
-        final float scale = context.getResources().getDisplayMetrics().density;
-        return (int) (dpValue * scale + 0.5f);
-    }
-
-    /**
-     * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
-     */
-    public static float pxTodip(Context context, float pxValue) {
-        final float scale = context.getResources().getDisplayMetrics().density;
-        return pxValue / scale + 0.5f;
-    }
-
-    /**
-     * 手机版本+系统版本
-     *
-     * @return 手机版本+系统版本
-     */
-    public static String getPhoneVersion() {
-        return Build.MODEL + ","
-                + Build.VERSION.SDK_INT + ","
-                + Build.VERSION.RELEASE;
-    }
-
-    /**
-     * 获得APP版本号
-     *
-     * @param context context
-     * @return 获得APP版本号
-     */
-    public static String getAppVersionName(Context context) {
-        String versionName = "";
-        try {
-            PackageManager pm = context.getPackageManager();
-            PackageInfo pi = pm.getPackageInfo(context.getPackageName(), 0);
-            versionName = pi.versionName;
-            if (versionName == null || versionName.length() <= 0) {
-                return "";
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return versionName;
+        return 0;
     }
 }
---
+++ app/src/main/java/net/oschina/app/improve/widget/TitleBar.java
@@ -0,0 +1,104 @@
+package net.oschina.app.improve.widget;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.RequiresApi;
+import android.support.annotation.StringRes;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import net.oschina.app.R;
+
+/**
+ * Created by JuQiu
+ * on 16/9/5.
+ */
+public class TitleBar extends FrameLayout {
+    private TextView mTitle;
+    private ImageView mIcon;
+
+    public TitleBar(Context context) {
+        super(context);
+        init(null, 0, 0);
+    }
+
+    public TitleBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs, 0, 0);
+    }
+
+    public TitleBar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(attrs, defStyleAttr, 0);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public TitleBar(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(attrs, defStyleAttr, defStyleRes);
+    }
+
+
+    private void init(AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        Context context = getContext();
+
+        LayoutInflater inflater = LayoutInflater.from(context);
+        inflater.inflate(R.layout.lay_title_bar, this, true);
+
+        mTitle = (TextView) findViewById(R.id.tv_title);
+        mIcon = (ImageView) findViewById(R.id.iv_icon);
+
+
+        if (attrs != null) {
+            // Load attributes
+            final TypedArray a = context.obtainStyledAttributes(
+                    attrs, R.styleable.TitleBar, defStyleAttr, defStyleRes);
+
+            String title = a.getString(R.styleable.TitleBar_aTitle);
+            Drawable drawable = a.getDrawable(R.styleable.TitleBar_aIcon);
+            a.recycle();
+
+            mTitle.setText(title);
+            mIcon.setImageDrawable(drawable);
+        } else {
+            mIcon.setVisibility(GONE);
+        }
+
+        // Set Background
+        setBackgroundColor(getResources().getColor(R.color.main_green));
+
+        // Init padding
+        setPadding(getLeft(), getTop() + getStatusHeight(context), getRight(), getBottom());
+    }
+
+    public void setTitle(@StringRes int titleRes) {
+        mTitle.setText(titleRes);
+    }
+
+    public void setIcon(@DrawableRes int iconRes) {
+        mIcon.setImageResource(iconRes);
+        mIcon.setVisibility(VISIBLE);
+    }
+
+    public static int getStatusHeight(Context context) {
+        int statusHeight = 0;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            try {
+                Class<?> clazz = Class.forName("com.android.internal.R$dimen");
+                Object object = clazz.newInstance();
+                int height = Integer.parseInt(clazz.getField("status_bar_height")
+                        .get(object).toString());
+                statusHeight = context.getResources().getDimensionPixelSize(height);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return statusHeight;
+    }
+}
