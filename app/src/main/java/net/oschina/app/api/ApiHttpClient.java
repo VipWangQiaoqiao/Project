cf31aef4695dc82959f3887c6d9a46857ade18e1
uni7corn <yaoqi@oschina.cn>
2016-10-26 10:37:53 +0800

    修改主机地址

--- app/src/main/java/net/oschina/app/api/ApiHttpClient.java
+++ app/src/main/java/net/oschina/app/api/ApiHttpClient.java
@@ -40,9 +40,9 @@ import cz.msebera.android.httpclient.protocol.HttpContext;
 @SuppressWarnings("WeakerAccess")
 public class ApiHttpClient {
 
-    public final static String HOST = "www.oschina.tk";
-    //private static String API_URL = "https://www.oschina.net/%s";
-    private static String API_URL = "http://192.168.1.10/%s";
+    public final static String HOST = "www.oschina.net";
+    private static String API_URL = "https://www.oschina.net/%s";
+    //private static String API_URL = "http://192.168.1.10/%s";
 
     private static AsyncHttpClient CLIENT;
 
