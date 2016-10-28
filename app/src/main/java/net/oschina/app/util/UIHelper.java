c573d3e2dee65af1655dea5a3f48ca567a0ead8e
uni7corn <yaoqi@oschina.cn>
2016-10-28 17:12:28 +0800

    sync the remote

--- app/src/main/java/net/oschina/app/util/UIHelper.java
+++ app/src/main/java/net/oschina/app/util/UIHelper.java
@@ -50,6 +50,7 @@ import net.oschina.app.fragment.FriendsFragment;
 import net.oschina.app.fragment.MessageDetailFragment;
 import net.oschina.app.fragment.QuestionTagFragment;
 import net.oschina.app.fragment.SoftWareTweetsFrament;
+import net.oschina.app.improve.account.activity.LoginActivity;
 import net.oschina.app.improve.account.manager.UserCacheManager;
 import net.oschina.app.improve.app.AppOperator;
 import net.oschina.app.improve.detail.activities.BlogDetailActivity;
@@ -124,8 +125,9 @@ public class UIHelper {
      * @param context
      */
     public static void showLoginActivity(Context context) {
-        Intent intent = new Intent(context, net.oschina.app.improve.account.activity.LoginActivity.class);
-        context.startActivity(intent);
+        LoginActivity.show(context);
+        // Intent intent = new Intent(context, net.oschina.app.improve.account.activity.LoginActivity.class);
+        // context.startActivity(intent);
     }
 
     /**
