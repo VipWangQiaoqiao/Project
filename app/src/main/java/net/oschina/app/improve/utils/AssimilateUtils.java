90ff00a3f98eb23ddbee915f1df167819c89b6d7
yaoqi <yaoqi@oschina.cn>
2016-11-22 18:21:00 +0800

    修复输入框正则表达式匹配不全的问题

--- app/src/main/java/net/oschina/app/improve/utils/AssimilateUtils.java
+++ app/src/main/java/net/oschina/app/improve/utils/AssimilateUtils.java
@@ -377,7 +377,7 @@ public class AssimilateUtils {
     }
 
     public static boolean machEmail(CharSequence email) {
-        String regex="([a-zA-Z0-9_\\-.]+)@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.)|(([a-zA-Z0-9\\-]+\\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})";
+        String regex="[\\w!#$%&'*+/=?^_`{|}~-]+(?:\\.[\\w!#$%&'*+/=?^_`{|}~-]+)*@(?:[\\w](?:[\\w-]*[\\w])?\\.)+[\\w](?:[\\w-]*[\\w])?";
         return Pattern.matches(regex, email);
     }
 
