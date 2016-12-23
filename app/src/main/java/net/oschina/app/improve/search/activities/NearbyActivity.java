9243a7204e0deb9ff104a91b33f73c7b5b5415c1
handoop <handoop4claire@gmail.com>
2016-12-23 18:41:27 +0800

    实现搜索附近的人

--- app/src/main/java/net/oschina/app/improve/search/activities/NearbyActivity.java
+++ app/src/main/java/net/oschina/app/improve/search/activities/NearbyActivity.java
@@ -1,10 +1,19 @@
 package net.oschina.app.improve.search.activities;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
+import android.support.design.widget.BottomSheetDialog;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.ViewGroup;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.baidu.location.BDLocation;
 import com.baidu.location.BDLocationListener;
@@ -21,12 +30,23 @@ import com.baidu.mapapi.radar.RadarUploadInfo;
 
 import net.oschina.app.R;
 import net.oschina.app.improve.account.AccountHelper;
+import net.oschina.app.improve.app.AppOperator;
 import net.oschina.app.improve.base.activities.BaseActivity;
 import net.oschina.app.improve.base.adapter.BaseRecyclerAdapter;
+import net.oschina.app.improve.bean.NearbyResult;
+import net.oschina.app.improve.bean.User;
+import net.oschina.app.improve.search.adapters.NearbyUserAdapter;
+import net.oschina.app.improve.user.activities.OtherUserHomeActivity;
+import net.oschina.app.improve.utils.DialogHelper;
+import net.oschina.app.improve.widget.BottomDialog;
 import net.oschina.app.improve.widget.RecyclerRefreshLayout;
 import net.oschina.app.improve.widget.SimplexToast;
 import net.oschina.app.util.TLog;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.Bind;
@@ -36,14 +56,19 @@ import butterknife.Bind;
  * Created by thanatosx on 2016/12/22.
  */
 
-public class NearbyActivity extends BaseActivity implements RadarSearchListener, BDLocationListener, RecyclerRefreshLayout.SuperRefreshLayoutListener, BaseRecyclerAdapter.OnItemClickListener {
+public class NearbyActivity extends BaseActivity implements RadarSearchListener, BDLocationListener,
+        RecyclerRefreshLayout.SuperRefreshLayoutListener, BaseRecyclerAdapter.OnItemClickListener, View.OnClickListener {
 
-    @Bind(R.id.recycler) RecyclerView mRecycler;
-    @Bind(R.id.layout_recycler_refresh) RecyclerRefreshLayout mRecyclerRefresh;
+    @Bind(R.id.recycler)
+    RecyclerView mRecycler;
+    @Bind(R.id.layout_recycler_refresh)
+    RecyclerRefreshLayout mRecyclerRefresh;
 
     private int mPageNum = 0;
     private LatLng mUserLatLng;
-    private BaseRecyclerAdapter mAdapter;
+    private Dialog mLoadingDialog;
+    private Dialog mSelectorDialog;
+    private BaseRecyclerAdapter<NearbyResult> mAdapter;
     private LocationClient mLocationClient;
     private RadarSearchManager mManager = RadarSearchManager.getInstance();
 
@@ -65,19 +90,11 @@ public class NearbyActivity extends BaseActivity implements RadarSearchListener,
                 R.color.swiperefresh_color1, R.color.swiperefresh_color2,
                 R.color.swiperefresh_color3, R.color.swiperefresh_color4);
 
-        mRecycler.setAdapter(mAdapter = new BaseRecyclerAdapter(this, BaseRecyclerAdapter.ONLY_FOOTER) {
-            @Override
-            protected RecyclerView.ViewHolder onCreateDefaultViewHolder(ViewGroup parent, int type) {
-                return null;
-            }
-
-            @Override
-            protected void onBindDefaultViewHolder(RecyclerView.ViewHolder holder, Object item, int position) {
-
-            }
-        });
+        mRecycler.setAdapter(mAdapter = new NearbyUserAdapter(this));
         mAdapter.setOnItemClickListener(this);
 
+        mLoadingDialog = DialogHelper.getProgressDialog(this, "正在加载...", false);
+        mLoadingDialog.show();
     }
 
     @Override
@@ -115,14 +132,38 @@ public class NearbyActivity extends BaseActivity implements RadarSearchListener,
         mLocationClient.start();
     }
 
+    private Dialog getSelectorDialog() {
+        if (mSelectorDialog == null) {
+            mSelectorDialog = new BottomDialog(this);
+            View view = LayoutInflater.from(this).inflate(R.layout.view_nearby_operator, null);
+            view.findViewById(R.id.tv_clear_opt).setOnClickListener(this);
+            mSelectorDialog.setContentView(view);
+        }
+        return mSelectorDialog;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_nearby_more, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_item_more:
+                getSelectorDialog().show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
         mLocationClient.stop();
         //移除监听
         mManager.removeNearbyInfoListener(this);
-        //清除用户信息
-        mManager.clearUserInfo();
         //释放资源
         mManager.destroy();
         mManager = null;
@@ -132,10 +173,15 @@ public class NearbyActivity extends BaseActivity implements RadarSearchListener,
      * 附近的人的监听
      *
      * @param result Radar Nearby Result
-     * @param error Radar Search Error
+     * @param error  Radar Search Error
      */
     @Override
     public void onGetNearbyInfoList(RadarNearbyResult result, RadarSearchError error) {
+        mLoadingDialog.cancel();
+        if (mPageNum == 0) {
+            mAdapter.clear();
+            mRecyclerRefresh.setRefreshing(false);
+        }
         if (error != RadarSearchError.RADAR_NO_ERROR) {
             SimplexToast.show(this, "没有获取到附近的人");
             mAdapter.clear();
@@ -143,20 +189,40 @@ public class NearbyActivity extends BaseActivity implements RadarSearchListener,
             return;
         }
         TLog.i("oschina", "Get Nearby Info List: Successful");
-        List<RadarNearbyInfo> infos =  result.infoList;
+        List<RadarNearbyInfo> infos = result.infoList;
+        List<NearbyResult> results = new ArrayList<>();
         for (RadarNearbyInfo info : infos) {
+            User user;
+            try {
+                String comments = URLDecoder.decode(info.comments, "UTF-8");
+                TLog.i("oschina", "Nearby Info List: " + comments);
+                user = AppOperator.createGson().fromJson(comments, User.class);
+            } catch (Exception e) {
+                e.printStackTrace();
+                user = new User();
+                user.setName("???");
+            }
+            NearbyResult.Nearby nearby = new NearbyResult.Nearby();
+            nearby.setDistance(info.distance);
+            nearby.setMobileName(info.mobileName);
+            nearby.setMobileOS(info.mobileOS);
+
+            results.add(new NearbyResult(user, nearby));
             TLog.i("oschina", String.format("comments: %s, distance: %s, mobile name: %s, mobile OS: %s, user id: %s",
                     info.comments, info.distance, info.mobileName, info.mobileOS, info.userID));
         }
+        mAdapter.addAll(results);
     }
 
     /**
      * 上传用户信息的监听
+     *
      * @param error Radar Search Error
      */
     @Override
     public void onGetUploadState(RadarSearchError error) {
         if (error != RadarSearchError.RADAR_NO_ERROR) {
+            mLoadingDialog.cancel();
             SimplexToast.show(this, "上传用户信息失败");
             return;
         }
@@ -166,14 +232,17 @@ public class NearbyActivity extends BaseActivity implements RadarSearchListener,
 
     /**
      * 清除用户信息的监听
+     *
      * @param error Radar Search Error
      */
     @Override
     public void onGetClearInfoState(RadarSearchError error) {
         if (error != RadarSearchError.RADAR_NO_ERROR) {
             TLog.i("oschina", "On Clear Radar Search Info State: ERROR!!");
+            SimplexToast.show(this, "清除失败");
             return;
         }
+        supportFinishAfterTransition();
         TLog.i("oschina", "On Clear Radar Search Info State: Successful");
     }
 
@@ -187,16 +256,20 @@ public class NearbyActivity extends BaseActivity implements RadarSearchListener,
         final int code = location.getLocType();
         switch (code) {
             case 62:
+                mLoadingDialog.cancel();
                 SimplexToast.show(this, "无法定位");
                 return;
             case 63:
+                mLoadingDialog.cancel();
                 SimplexToast.show(this, "网络异常");
                 return;
             case 167:
+                mLoadingDialog.cancel();
                 SimplexToast.show(this, "服务器定位失败，请检查权限");
                 return;
         }
         if (code >= 501) {
+            mLoadingDialog.cancel();
             SimplexToast.show(this, "非法或无效的APP KEY");
             return;
         }
@@ -207,9 +280,37 @@ public class NearbyActivity extends BaseActivity implements RadarSearchListener,
         mManager.setUserID(String.valueOf(AccountHelper.getUserId()));
         //上传位置
         RadarUploadInfo info = new RadarUploadInfo();
-        info.comments = AccountHelper.getUser().getName();
-        info.pt = mUserLatLng = new LatLng(location.getLatitude(), location.getLongitude());
-        mManager.uploadInfoRequest(info);
+        User user = AccountHelper.getUser();
+        try {
+            String company = "";
+            String position = "";
+            if (user.getMore() != null) {
+                company = user.getMore().getCompany();
+                position = user.getMore().getPosition();
+            }
+            company = TextUtils.isEmpty(company) ? "" : company;
+            position = TextUtils.isEmpty(position) ? "" : position;
+            String comments = String.format(
+                    "{" +
+                            "\"id\":\"%s\"," +
+                            "\"name\":\"%s\"," +
+                            "\"portrait\":\"%s\"," +
+                            "\"gender\":\"%s\"," +
+                            "\"more\":{" +
+                            "\"company\":\"%s\"," +
+                            "\"position\":\"%s\"" +
+                            "}" +
+                            "}"
+                    , user.getId(), user.getName(), user.getPortrait(), user.getGender(), company, position);
+            comments = URLEncoder.encode(comments, "UTF-8");
+            TLog.i("oschina", comments);
+            info.comments = comments;
+            info.pt = mUserLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+            mManager.uploadInfoRequest(info);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            SimplexToast.show(this, "上传用户信息失败");
+        }
     }
 
     @Override
@@ -219,19 +320,32 @@ public class NearbyActivity extends BaseActivity implements RadarSearchListener,
 
     @Override
     public void onLoadMore() {
+        TLog.i("oschina", "Nearby On Load More: " + mPageNum);
         requestData(++mPageNum);
     }
 
     private void requestData(int pageNum) {
         //构造请求参数，其中centerPt是自己的位置坐标
         RadarNearbySearchOption option = new RadarNearbySearchOption()
-                .centerPt(mUserLatLng).pageNum(pageNum).radius(5000).pageCapacity(20);
+                .centerPt(mUserLatLng).pageNum(pageNum).radius(20000).pageCapacity(20);
         //发起查询请求
         mManager.nearbyInfoRequest(option);
     }
 
     @Override
     public void onItemClick(int position, long itemId) {
+        NearbyResult result = mAdapter.getItem(position);
+        if (result == null) return;
+        OtherUserHomeActivity.show(this, result.getUser());
+    }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tv_clear_opt:
+                //清除用户信息
+                mManager.clearUserInfo();
+                break;
+        }
     }
 }
---
+++ app/src/main/java/net/oschina/app/improve/search/adapters/NearbyUserAdapter.java
@@ -0,0 +1,101 @@
+package net.oschina.app.improve.search.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import net.oschina.app.R;
+import net.oschina.app.improve.base.adapter.BaseRecyclerAdapter;
+import net.oschina.app.improve.bean.NearbyResult;
+import net.oschina.app.improve.bean.User;
+import net.oschina.app.util.StringUtils;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import de.hdodenhof.circleimageview.CircleImageView;
+
+/**
+ * Created by thanatos on 16/10/24.
+ */
+
+public class NearbyUserAdapter extends BaseRecyclerAdapter<NearbyResult> {
+
+    public NearbyUserAdapter(Context context) {
+        this(context, ONLY_FOOTER);
+    }
+
+    public NearbyUserAdapter(Context context, int mode) {
+        super(context, mode);
+    }
+
+    @Override
+    protected RecyclerView.ViewHolder onCreateDefaultViewHolder(ViewGroup parent, int type) {
+        return new ViewHolder(LayoutInflater.from(mContext)
+                .inflate(R.layout.list_item_nearby_user, parent, false));
+    }
+
+    @Override
+    protected void onBindDefaultViewHolder(RecyclerView.ViewHolder h, NearbyResult item, int position) {
+        ViewHolder holder = (ViewHolder) h;
+
+        if (item.getUser() != null) {
+            Glide.with(mContext)
+                    .load(item.getUser().getPortrait())
+                    .asBitmap()
+                    .placeholder(R.mipmap.widget_dface)
+                    .error(R.mipmap.widget_dface)
+                    .into(holder.mViewPortrait);
+            holder.mViewNick.setText(item.getUser().getName());
+            holder.mViewGender.setVisibility(View.VISIBLE);
+            switch (item.getUser().getGender()) {
+                case User.GENDER_FEMALE:
+                    holder.mViewGender.setImageResource(R.mipmap.ic_female);
+                    break;
+                case User.GENDER_MALE:
+                    holder.mViewGender.setImageResource(R.mipmap.ic_male);
+                    break;
+                default:
+                    holder.mViewGender.setVisibility(View.GONE);
+            }
+            User.More more = item.getUser().getMore();
+            if (more != null) {
+                holder.mViewPosition.setText(more.getCompany() + " " + more.getPosition());
+            }else {
+                holder.mViewPosition.setText("??? ???");
+            }
+        }else {
+            holder.mViewPortrait.setImageResource(R.mipmap.widget_dface);
+            holder.mViewNick.setText("???");
+            holder.mViewGender.setVisibility(View.GONE);
+            holder.mViewPosition.setText("??? ???");
+        }
+
+        if (item.getNearby() != null) {
+            holder.mViewDistance.setText(StringUtils.formatDistance(item.getNearby().getDistance()));
+        }else {
+            holder.mViewDistance.setText("未知距离");
+        }
+
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        @Bind(R.id.iv_portrait) CircleImageView mViewPortrait;
+        @Bind(R.id.tv_nick)     TextView mViewNick;
+        @Bind(R.id.tv_position) TextView mViewPosition;
+        @Bind(R.id.tv_distance) TextView mViewDistance;
+        @Bind(R.id.iv_gender)   ImageView mViewGender;
+
+        public ViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+    }
+
+}
