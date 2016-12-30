5d2699c93954d4b59a458c9db080586dff6ad82b
handoop <handoop4claire@gmail.com>
2016-12-30 17:00:55 +0800

    消除无用import

--- app/src/main/java/net/oschina/app/improve/user/activities/UserMessageActivity.java
+++ app/src/main/java/net/oschina/app/improve/user/activities/UserMessageActivity.java
@@ -6,7 +6,6 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.util.Log;
 
 import net.oschina.app.R;
 import net.oschina.app.improve.base.activities.BaseBackActivity;
