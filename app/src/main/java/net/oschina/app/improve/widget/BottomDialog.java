82183e6f751db0edd0abc28225585fab05d301b1
haibin <huanghaibin_dev@163.com>
2017-05-09 14:52:00 +0800

    解决虚拟按键手机BottomSheetDialog键盘高度自适应bug

--- app/src/main/java/net/oschina/app/improve/widget/BottomDialog.java
+++ app/src/main/java/net/oschina/app/improve/widget/BottomDialog.java
@@ -18,11 +18,9 @@ import android.view.WindowManager;
 public class BottomDialog extends BottomSheetDialog {
 
     private BottomSheetBehavior behavior;
-    private boolean isTranslucentStatus;
 
     public BottomDialog(@NonNull Context context, boolean isTranslucentStatus) {
         super(context);
-        this.isTranslucentStatus = isTranslucentStatus;
         Window window = getWindow();
         if (window != null) {
             window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE |
